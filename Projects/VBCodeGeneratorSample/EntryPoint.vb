284b0ef040405034c852f8ec0f7ea967f04eab77
Tom DuPont <tdupont750@gmail.com>
2008-05-28 18:00:58 +0000

    Added BusinessObject.cst logic to demo.

--- Projects/VBCodeGeneratorSample/EntryPoint.vb
+++ Projects/VBCodeGeneratorSample/EntryPoint.vb
@@ -3,5 +3,8 @@ Module EntryPoint
         Dim stringhashtable As VBCodeGeneratorSample.StringHashtable = New VBCodeGeneratorSample.StringHashtable
         Dim stringinthashtable As VBCodeGeneratorSample.StringIntegerHashtable = New VBCodeGeneratorSample.StringIntegerHashtable
         Dim inthashtable As VBCodeGeneratorSample.IntegerHashtable = New VBCodeGeneratorSample.IntegerHashtable
+
+        Dim product As Product = New Product("BG-01")
+        Dim supplier As Supplier = New Supplier()
     End Sub
 End Module
