c058fa9669b01199a0468f58bd9362906a7fa0b0
tdupont750 <tdupont750@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2008-05-30 15:06:40 +0000

    Updated BusinessObject.cst and rebuilt.

--- trunk/Projects/VBCodeGeneratorSample/EntryPoint.vb
+++ trunk/Projects/VBCodeGeneratorSample/EntryPoint.vb
@@ -1,10 +1,10 @@
 Module EntryPoint
     Sub Main()
-        Dim stringhashtable As VBCodeGeneratorSample.StringHashtable = New VBCodeGeneratorSample.StringHashtable
-        Dim stringinthashtable As VBCodeGeneratorSample.StringIntegerHashtable = New VBCodeGeneratorSample.StringIntegerHashtable
-        Dim inthashtable As VBCodeGeneratorSample.IntegerHashtable = New VBCodeGeneratorSample.IntegerHashtable
+        Dim stringhashtable As StringHashtable = New StringHashtable
+        Dim stringinthashtable As StringIntegerHashtable = New StringIntegerHashtable
+        Dim inthashtable As IntegerHashtable = New IntegerHashtable
 
-        Dim product As Product = New Product("BG-01")
-        Dim supplier As Supplier = New Supplier()
+        Dim product As VBCodeGeneratorSample.Product = New VBCodeGeneratorSample.Product("BG-01")
+        Dim supplier As VBCodeGeneratorSample.Supplier = New VBCodeGeneratorSample.Supplier()
     End Sub
 End Module
--- trunk/Projects/VBCodeGeneratorSample/Product.vb
+++ trunk/Projects/VBCodeGeneratorSample/Product.vb
@@ -1,8 +1,10 @@
 Imports System
+Imports System.Collections.Generic
+Imports System.Text
 Imports System.Data
 Imports System.Data.SqlClient
-Imports System.Text
 
+Namespace VBCodeGeneratorSample
 #Region "Product"
 ''' <summary>
 ''' This object represents the properties and methods of a Product.
@@ -167,4 +169,5 @@ Public Class Product
 	
 End Class
 #End Region
+End Namespace
 
