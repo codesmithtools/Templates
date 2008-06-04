b005e69655067c8857f018d90d4c4a2b5488149e
Reggie Bradshaw <regbradii@users.noreply.github.com>
2008-06-04 22:59:10 +0000

    Committing actual Code for VB Engine Sample. Need to recreate Project and commit.

---
+++ Projects/VisualBasic/EngineSample/EntryPoint.vb
@@ -0,0 +1,37 @@
+﻿Imports System
+Imports CodeSmith.Engine
+Imports SchemaExplorer
+
+
+
+
+Module EntryPoint
+
+    Sub Main()
+        Dim compiler As CodeTemplateCompiler
+        compiler = New CodeTemplateCompiler("..\\..\\StoredProcedures.cst")
+        compiler.Compile()
+        If compiler.Errors.Count = 0 Then
+            Dim template As CodeTemplate
+            template = compiler.CreateInstance()
+
+            Dim database As DatabaseSchema
+            database = New DatabaseSchema(New SqlSchemaProvider(), "Server=(local);Database=Petshop;Integrated Security=true;")
+            Dim table As TableSchema
+            table = database.Tables("Inventory")
+
+            template.SetProperty("SourceTable", table)
+            template.SetProperty("IncludeDrop", False)
+            template.SetProperty("InsertPrefix", "Insert")
+
+            template.Render(Console.Out)
+        Else
+            Dim i As Integer
+            For i = 0 To compiler.Errors.Count
+                Console.Error.WriteLine(compiler.Errors(i).ToString())
+                Console.Read()
+            Next
+        End If
+    End Sub
+
+End Module
