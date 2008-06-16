e788177de4d3250bdff5c6af0ed5352eb28bc8d8
regbradii@gmail.com <regbradii@gmail.com@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2008-06-16 21:11:50 +0000

    APISample VB Version Fixed for Name change

--- trunk/Projects/VisualBasic/APISample/EntryPoint.vb
+++ trunk/Projects/VisualBasic/APISample/EntryPoint.vb
@@ -5,33 +5,35 @@ Imports SchemaExplorer
 
 
 
-Module EntryPoint
+Namespace APISample
+    Module EntryPoint
+        Sub Main()
+            Dim compiler As CodeTemplateCompiler
+            compiler = New CodeTemplateCompiler("..\\..\\StoredProcedures.cst")
+            compiler.Compile()
+            If compiler.Errors.Count = 0 Then
+                Dim template As CodeTemplate
+                template = compiler.CreateInstance()
 
-    Sub Main()
-        Dim compiler As CodeTemplateCompiler
-        compiler = New CodeTemplateCompiler("..\\..\\StoredProcedures.cst")
-        compiler.Compile()
-        If compiler.Errors.Count = 0 Then
-            Dim template As CodeTemplate
-            template = compiler.CreateInstance()
+                Dim database As DatabaseSchema
+                database = New DatabaseSchema(New SqlSchemaProvider(), "Server=(local);Database=Petshop;Integrated Security=true;")
+                Dim table As TableSchema
+                table = database.Tables("Inventory")
 
-            Dim database As DatabaseSchema
-            database = New DatabaseSchema(New SqlSchemaProvider(), "Server=(local);Database=Petshop;Integrated Security=true;")
-            Dim table As TableSchema
-            table = database.Tables("Inventory")
+                template.SetProperty("SourceTable", table)
+                template.SetProperty("IncludeDrop", False)
+                template.SetProperty("InsertPrefix", "Insert")
 
-            template.SetProperty("SourceTable", table)
-            template.SetProperty("IncludeDrop", False)
-            template.SetProperty("InsertPrefix", "Insert")
+                template.Render(Console.Out)
+            Else
+                Dim i As Integer
+                For i = 0 To compiler.Errors.Count
+                    Console.Error.WriteLine(compiler.Errors(i).ToString())
+                    Console.Read()
+                Next
+            End If
+        End Sub
+    End Module
 
-            template.Render(Console.Out)
-        Else
-            Dim i As Integer
-            For i = 0 To compiler.Errors.Count
-                Console.Error.WriteLine(compiler.Errors(i).ToString())
-                Console.Read()
-            Next
-        End If
-    End Sub
 
-End Module
+End Namespace
