801ba2ca4c272e31aab6334c10a851925161be4f
tdupont750 <tdupont750@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2008-06-25 17:02:41 +0000

    Updated SearchCriteria

--- trunk/Templates/Frameworks/NHibernate/VisualBasic/Helpers/NHibernateHelper.vb
+++ trunk/Templates/Frameworks/NHibernate/VisualBasic/Helpers/NHibernateHelper.vb
@@ -212,20 +212,12 @@ Public Class NHibernateHelper
     End Function
     Public Function GetMethodDeclaration(ByVal sc As SearchCriteria) As String
         Dim result As New StringBuilder()
-        result.Append(GetMethodName(sc))
+        result.Append(sc.MethodName)
         result.Append("(")
         result.Append(GetMethodParameters(sc.Items, True))
         result.Append(")")
         Return result.ToString()
     End Function
-	Public Function GetMethodName(ByVal sc As SearchCriteria) As String
-		Dim result As New StringBuilder()
-        result.Append("GetBy")
-        For Each mcs As MemberColumnSchema In sc.Items
-            result.Append(GetPropertyName(mcs.Name))
-        Next
-		Return result.ToString()
-	End Function
     Public Function GetPrimaryKeyCallParameters(ByVal mcsList As List(Of MemberColumnSchema)) As String
         Dim result As New System.Text.StringBuilder()
         Dim x As Integer = 0
@@ -261,211 +253,319 @@ Public Class NHibernateHelper
         End If
         Return result
     End Function
+	
+	Public Function ContainsForeignKey(ByVal sc As SearchCriteria, ByVal tsc As TableSchemaCollection) As Boolean
+	For Each ts As TableSchema In tsc
+		For Each tks As TableKeySchema In ts.PrimaryKeys
+			For Each mcs As MemberColumnSchema In sc.Items
+				If tks.PrimaryKeyMemberColumns.Contains(mcs) Then
+					Return True
+				End If
+			Next
+		Next
+	Next
+	Return False
+	End Function
+
 End Class
 
-#Region "SearchCriteria Class"
+#region "SearchCriteria Class"
 
 Public Class SearchCriteria
-#Region "Static Content"
+	#region "Static Content"
 
-    Public Shared Function GetAllSearchCriteria(ByVal table As TableSchema) As List(Of SearchCriteria)
-        Dim tsc As New TableSearchCriteria(table)
-        Return tsc.GetAllSearchCriteria()
-    End Function
-    Public Shared Function GetPrimaryKeySearchCriteria(ByVal table As TableSchema) As List(Of SearchCriteria)
-        Dim tsc As New TableSearchCriteria(table)
-        Return tsc.GetPrimaryKeySearchCriteria()
-    End Function
-    Public Shared Function GetForeignKeySearchCriteria(ByVal table As TableSchema) As List(Of SearchCriteria)
-        Dim tsc As New TableSearchCriteria(table)
-        Return tsc.GetForeignKeySearchCriteria()
-    End Function
-    Public Shared Function GetIndexSearchCriteria(ByVal table As TableSchema) As List(Of SearchCriteria)
-        Dim tsc As New TableSearchCriteria(table)
-        Return tsc.GetIndexSearchCriteria()
-    End Function
+	Public Shared Function GetAllSearchCriteria(ByVal table As TableSchema, ByVal extendedProperty As String) As List(Of SearchCriteria)
+		Dim tsc As New TableSearchCriteria(table, extendedProperty)
+		Return tsc.GetAllSearchCriteria()
+	End Function
+	Public Shared Function GetAllSearchCriteria(ByVal table As TableSchema) As List(Of SearchCriteria)
+		Dim tsc As New TableSearchCriteria(table)
+		Return tsc.GetAllSearchCriteria()
+	End Function
+
+	Public Shared Function GetPrimaryKeySearchCriteria(ByVal table As TableSchema, ByVal extendedProperty As String) As List(Of SearchCriteria)
+		Dim tsc As New TableSearchCriteria(table, extendedProperty)
+		Return tsc.GetPrimaryKeySearchCriteria()
+	End Function
+	Public Shared Function GetPrimaryKeySearchCriteria(ByVal table As TableSchema) As List(Of SearchCriteria)
+		Dim tsc As New TableSearchCriteria(table)
+		Return tsc.GetPrimaryKeySearchCriteria()
+	End Function
+
+	Public Shared Function GetForeignKeySearchCriteria(ByVal table As TableSchema, ByVal extendedProperty As String) As List(Of SearchCriteria)
+		Dim tsc As New TableSearchCriteria(table, extendedProperty)
+		Return tsc.GetForeignKeySearchCriteria()
+	End Function
+	Public Shared Function GetForeignKeySearchCriteria(ByVal table As TableSchema) As List(Of SearchCriteria)
+		Dim tsc As New TableSearchCriteria(table)
+		Return tsc.GetForeignKeySearchCriteria()
+	End Function
+
+	Public Shared Function GetIndexSearchCriteria(ByVal table As TableSchema, ByVal extendedProperty As String) As List(Of SearchCriteria)
+		Dim tsc As New TableSearchCriteria(table, extendedProperty)
+		Return tsc.GetIndexSearchCriteria()
+	End Function
+	Public Shared Function GetIndexSearchCriteria(ByVal table As TableSchema) As List(Of SearchCriteria)
+		Dim tsc As New TableSearchCriteria(table)
+		Return tsc.GetIndexSearchCriteria()
+	End Function
 #End Region
 
-#Region "Declarations"
+	#region "Declarations"
 
-    Protected mcsList As List(Of MemberColumnSchema)
+	Protected mcsList As List(Of MemberColumnSchema)
+	Protected _methodNameGenerationMode As MethodNameGenerationMode = MethodNameGenerationMode.[Default]
+	Protected _methodName As String = [String].Empty
 #End Region
 
-#Region "Constructors"
+	#region "Constructors"
 
-    Protected Sub New()
-        mcsList = New List(Of MemberColumnSchema)()
-    End Sub
-    Protected Sub New(ByVal mcsList As List(Of MemberColumnSchema))
-        Me.mcsList = mcsList
-    End Sub
+	Protected Sub New()
+		mcsList = New List(Of MemberColumnSchema)()
+	End Sub
+	Protected Sub New(ByVal mcsList As List(Of MemberColumnSchema))
+		Me.mcsList = mcsList
+	End Sub
 #End Region
 
-#Region "Methods"
+	#region "Methods"
 
-    Protected Sub Add(ByVal item As MemberColumnSchema)
-        mcsList.Add(item)
-    End Sub
-    Public Overloads Overrides Function ToString() As String
-        Dim sb As New StringBuilder()
-        Dim isFirst As Boolean = True
+	''' <summary>
+	''' Sets MethodName to default generation: "GetBy{0}{1}{n}"
+	''' </summary>
+	Public Sub SetMethodNameGeneration()
+		_methodNameGenerationMode = MethodNameGenerationMode.[Default]
 
-        For Each mcs As MemberColumnSchema In mcsList
-            If isFirst Then
-                isFirst = False
-            Else
-                sb.Append(" & ")
-            End If
-            sb.Append(mcs.Name)
-        Next
+		GenerateMethodName("GetBy", [String].Empty, [String].Empty)
+	End Sub
+	''' <summary>
+	''' Sets MethodName to be value of the specified Extended Property from the database.
+	''' </summary>
+	''' <param name="extendedProperty">Value of the Extended Property.</param>
+	Public Sub SetMethodNameGeneration(ByVal extendedProperty As String)
+		_methodNameGenerationMode = MethodNameGenerationMode.ExtendedProperty
 
-        Return sb.ToString()
-    End Function
-    Public Function ContainsForeignKey(ByVal tsc As TableSchemaCollection) As Boolean
-        For Each ts As TableSchema In tsc
-            For Each tks As TableKeySchema In ts.PrimaryKeys
-                For Each mcs As MemberColumnSchema In mcsList
-                    If tks.PrimaryKeyMemberColumns.Contains(mcs) Then
-                        Return True
-                    End If
-                Next
-            Next
-        Next
-        Return False
-    End Function
+		_methodName = extendedProperty
+	End Sub
+	''' <summary>
+	''' Sets MethodName to custom generation: "{prefix}{0}{delimeter}{1}{suffix}"
+	''' </summary>
+	''' <param name="prefix">Method Prefix</param>
+	''' <param name="delimeter">Column Delimeter</param>
+	''' <param name="suffix">Method Suffix</param>
+	Public Sub SetMethodNameGeneration(ByVal prefix As String, ByVal delimeter As String, ByVal suffix As String)
+		_methodNameGenerationMode = MethodNameGenerationMode.[Custom]
+
+		GenerateMethodName(prefix, delimeter, suffix)
+	End Sub
+
+	Public Overloads Overrides Function ToString() As String
+		If [String].IsNullOrEmpty(_methodName) Then
+			SetMethodNameGeneration()
+		End If
+
+		Return _methodName
+	End Function
+
+	Protected Sub Add(ByVal item As MemberColumnSchema)
+		mcsList.Add(item)
+	End Sub
+	Protected Sub GenerateMethodName(ByVal prefix As String, ByVal delimeter As String, ByVal suffix As String)
+		Dim sb As New StringBuilder()
+		Dim isFirst As Boolean = True
+
+		sb.Append(prefix)
+		For Each mcs As MemberColumnSchema In mcsList
+			If isFirst Then
+				isFirst = False
+			Else
+				sb.Append(delimeter)
+			End If
+			sb.Append(mcs.Name)
+		Next
+		sb.Append(suffix)
+
+		_methodName = sb.ToString()
+	End Sub
 #End Region
 
-#Region "Properties"
+	#region "Properties"
 
-    Public ReadOnly Property Items() As List(Of MemberColumnSchema)
-        Get
-            Return mcsList
-        End Get
-    End Property
-    Public ReadOnly Property IsAllPrimaryKeys() As Boolean
-        Get
-            Dim result As Boolean = True
-            For Each msc As MemberColumnSchema In mcsList
-                If Not msc.IsPrimaryKeyMember Then
-                    result = False
-                    Exit For
-                End If
-            Next
-            Return result
-        End Get
-    End Property
-    Protected ReadOnly Property Key() As String
-        Get
-            Return Me.ToString()
-        End Get
-    End Property
+	Public ReadOnly Property Items() As List(Of MemberColumnSchema)
+		Get
+			Return mcsList
+		End Get
+	End Property
+	Public ReadOnly Property IsAllPrimaryKeys() As Boolean
+		Get
+			Dim result As Boolean = True
+			For Each msc As MemberColumnSchema In mcsList
+				If Not msc.IsPrimaryKeyMember Then
+					result = False
+					Exit For
+				End If
+			Next
+			Return result
+		End Get
+	End Property
+	Public ReadOnly Property MethodName() As String
+		Get
+			Return Me.ToString()
+		End Get
+	End Property
+	Public ReadOnly Property MethodNameGeneration() As MethodNameGenerationMode
+		Get
+			Return _methodNameGenerationMode
+		End Get
+	End Property
+
+	Protected ReadOnly Property Key() As String
+		Get
+			Dim sb As New StringBuilder()
+
+			For Each mcs As MemberColumnSchema In mcsList
+				sb.Append(mcs.Name)
+			Next
+
+			Return sb.ToString()
+		End Get
+	End Property
 #End Region
 
-#Region "Internal Classes"
+	#region "Enums & Classes"
 
-    Friend Class TableSearchCriteria
-#Region "Declarations"
+	Public Enum MethodNameGenerationMode
+		[Default]
+		ExtendedProperty
+		[Custom]
+	End Enum
 
-        Protected _table As TableSchema
+	Friend Class TableSearchCriteria
+		#region "Declarations"
+
+		Protected _table As TableSchema
+		Protected extendedProperty As String = "cs_CriteriaName"
 #End Region
 
-#Region "Constructor"
+		#region "Constructor"
 
-        Public Sub New(ByVal sourceTable As TableSchema)
-            Me._table = sourceTable
-        End Sub
+		Public Sub New(ByVal sourceTable As TableSchema)
+			Me._table = sourceTable
+		End Sub
+		Public Sub New(ByVal sourceTable As TableSchema, ByVal extendedProperty As String)
+			Me.New(sourceTable)
+			Me.extendedProperty = extendedProperty
+		End Sub
 #End Region
 
-#Region "Methods"
+		#region "Methods"
 
-        Public Function GetAllSearchCriteria() As List(Of SearchCriteria)
-            Dim map As New Dictionary(Of String, SearchCriteria)()
+		Public Function GetAllSearchCriteria() As List(Of SearchCriteria)
+			Dim map As New Dictionary(Of String, SearchCriteria)()
 
-            GetPrimaryKeySearchCriteria(map)
-            GetForeignKeySearchCriteria(map)
-            GetIndexSearchCriteria(map)
+			GetPrimaryKeySearchCriteria(map)
+			GetForeignKeySearchCriteria(map)
+			GetIndexSearchCriteria(map)
 
-            Return GetResultsFromMap(map)
-        End Function
-        Public Function GetPrimaryKeySearchCriteria() As List(Of SearchCriteria)
-            Dim map As New Dictionary(Of String, SearchCriteria)()
+			Return GetResultsFromMap(map)
+		End Function
+		Public Function GetPrimaryKeySearchCriteria() As List(Of SearchCriteria)
+			Dim map As New Dictionary(Of String, SearchCriteria)()
 
-            GetPrimaryKeySearchCriteria(map)
+			GetPrimaryKeySearchCriteria(map)
 
-            Return GetResultsFromMap(map)
-        End Function
-        Public Function GetForeignKeySearchCriteria() As List(Of SearchCriteria)
-            Dim map As New Dictionary(Of String, SearchCriteria)()
+			Return GetResultsFromMap(map)
+		End Function
+		Public Function GetForeignKeySearchCriteria() As List(Of SearchCriteria)
+			Dim map As New Dictionary(Of String, SearchCriteria)()
 
-            GetForeignKeySearchCriteria(map)
+			GetForeignKeySearchCriteria(map)
 
-            Return GetResultsFromMap(map)
-        End Function
-        Public Function GetIndexSearchCriteria() As List(Of SearchCriteria)
-            Dim map As New Dictionary(Of String, SearchCriteria)()
+			Return GetResultsFromMap(map)
+		End Function
+		Public Function GetIndexSearchCriteria() As List(Of SearchCriteria)
+			Dim map As New Dictionary(Of String, SearchCriteria)()
 
-            GetIndexSearchCriteria(map)
+			GetIndexSearchCriteria(map)
 
-            Return GetResultsFromMap(map)
-        End Function
+			Return GetResultsFromMap(map)
+		End Function
 
-        Protected Sub GetPrimaryKeySearchCriteria(ByVal map As Dictionary(Of String, SearchCriteria))
-            Dim mcsList As New List(Of MemberColumnSchema)(table.PrimaryKey.MemberColumns.ToArray())
-            Dim searchCriteria As New SearchCriteria(mcsList)
-            AddToMap(map, searchCriteria)
-        End Sub
-        Protected Sub GetForeignKeySearchCriteria(ByVal map As Dictionary(Of String, SearchCriteria))
-            For Each tks As TableKeySchema In table.ForeignKeys
-                Dim searchCriteria As New SearchCriteria()
-                For Each mcs As MemberColumnSchema In tks.ForeignKeyMemberColumns
-                    If mcs.Table.Equals(table) Then
-                        searchCriteria.Add(mcs)
-                    End If
-                Next
-                AddToMap(map, searchCriteria)
-            Next
-        End Sub
-        Protected Sub GetIndexSearchCriteria(ByVal map As Dictionary(Of String, SearchCriteria))
-            For Each indexSchema As IndexSchema In table.Indexes
-                Dim searchCriteria As New SearchCriteria()
-                For Each mcs As MemberColumnSchema In indexSchema.MemberColumns
-                    If mcs.Table.Equals(table) Then
-                        searchCriteria.Add(mcs)
-                    End If
-                Next
-                AddToMap(map, searchCriteria)
-            Next
-        End Sub
+		Protected Sub GetPrimaryKeySearchCriteria(ByVal map As Dictionary(Of String, SearchCriteria))
+			Dim mcsList As New List(Of MemberColumnSchema)(table.PrimaryKey.MemberColumns.ToArray())
+			Dim searchCriteria As New SearchCriteria(mcsList)
 
-        Protected Function AddToMap(ByVal map As Dictionary(Of String, SearchCriteria), ByVal searchCriteria As SearchCriteria) As Boolean
-            Dim key As String = searchCriteria.Key
-            Dim result As Boolean = (searchCriteria.Items.Count > 0 AndAlso Not map.ContainsKey(key))
+			If table.PrimaryKey.ExtendedProperties.Contains(extendedProperty) Then
+				If Not [String].IsNullOrEmpty(extendedProperty) AndAlso table.PrimaryKey.ExtendedProperties.Contains(extendedProperty) AndAlso table.PrimaryKey.ExtendedProperties(extendedProperty).Value IsNot Nothing Then
+					searchCriteria.SetMethodNameGeneration(table.PrimaryKey.ExtendedProperties(extendedProperty).Value.ToString())
+				End If
+			End If
 
-            If result Then
-                map.Add(key, searchCriteria)
-            End If
+			AddToMap(map, searchCriteria)
+		End Sub
+		Protected Sub GetForeignKeySearchCriteria(ByVal map As Dictionary(Of String, SearchCriteria))
+			For Each tks As TableKeySchema In table.ForeignKeys
+				Dim searchCriteria As New SearchCriteria()
+				For Each mcs As MemberColumnSchema In tks.ForeignKeyMemberColumns
+					If mcs.Table.Equals(table) Then
+						searchCriteria.Add(mcs)
+					End If
+				Next
 
-            Return result
-        End Function
-        Protected Function GetResultsFromMap(ByVal map As Dictionary(Of String, SearchCriteria)) As List(Of SearchCriteria)
-            Dim result As New List(Of SearchCriteria)()
-            For Each kvp As KeyValuePair(Of String, SearchCriteria) In map
-                result.Add(kvp.Value)
-            Next
-            Return result
-        End Function
+				If Not [String].IsNullOrEmpty(extendedProperty) AndAlso tks.ExtendedProperties.Contains(extendedProperty) AndAlso tks.ExtendedProperties(extendedProperty).Value IsNot Nothing Then
+					searchCriteria.SetMethodNameGeneration(tks.ExtendedProperties(extendedProperty).Value.ToString())
+				End If
+
+				AddToMap(map, searchCriteria)
+			Next
+		End Sub
+		Protected Sub GetIndexSearchCriteria(ByVal map As Dictionary(Of String, SearchCriteria))
+			For Each indexSchema As IndexSchema In table.Indexes
+				Dim searchCriteria As New SearchCriteria()
+				For Each mcs As MemberColumnSchema In indexSchema.MemberColumns
+					If mcs.Table.Equals(table) Then
+						searchCriteria.Add(mcs)
+					End If
+				Next
+
+				If Not [String].IsNullOrEmpty(extendedProperty) AndAlso indexSchema.ExtendedProperties.Contains(extendedProperty) AndAlso indexSchema.ExtendedProperties(extendedProperty).Value IsNot Nothing Then
+					searchCriteria.SetMethodNameGeneration(indexSchema.ExtendedProperties(extendedProperty).Value.ToString())
+				End If
+
+				AddToMap(map, searchCriteria)
+			Next
+		End Sub
+
+		Protected Function AddToMap(ByVal map As Dictionary(Of String, SearchCriteria), ByVal searchCriteria As SearchCriteria) As Boolean
+			Dim key As String = searchCriteria.Key
+			Dim result As Boolean = (searchCriteria.Items.Count > 0 AndAlso Not map.ContainsKey(key))
+
+			If result Then
+				map.Add(key, searchCriteria)
+			End If
+
+			Return result
+		End Function
+		Protected Function GetResultsFromMap(ByVal map As Dictionary(Of String, SearchCriteria)) As List(Of SearchCriteria)
+			Dim result As New List(Of SearchCriteria)()
+			For Each kvp As KeyValuePair(Of String, SearchCriteria) In map
+				result.Add(kvp.Value)
+			Next
+			Return result
+		End Function
 #End Region
 
-#Region "Properties"
+		#region "Properties"
 
-        Public ReadOnly Property Table() As TableSchema
-            Get
-                Return _table
-            End Get
-        End Property
+		Public ReadOnly Property Table() As TableSchema
+			Get
+				Return _table
+			End Get
+		End Property
 #End Region
-    End Class
+	End Class
 #End Region
 End Class
 #End Region
+
 
