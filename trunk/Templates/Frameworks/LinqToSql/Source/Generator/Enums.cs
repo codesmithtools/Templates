f04d1b9a5fe6a56a057479acc4058e3bbcacf68e
tdupont750 <tdupont750@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2008-10-06 18:28:45 +0000

    Added Enums.cs

---
+++ trunk/Templates/Frameworks/LinqToSql/Source/Generator/Enums.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqToSqlShared.Generator
+{
+    public enum FrameworkEnum
+    {
+        v35 = 1,
+        v35_SP1 = 2
+    }
+
+    public enum TableNamingEnum
+    {
+        Plural = 1,
+        Singluar = 2,
+        Mixed = 3
+    }
+
+    public enum EntityNamingEnum
+    {
+        Preserve = 0,
+        Plural = 1,
+        Singular = 2
+    }
+
+    public enum AssociationNamingEnum
+    {
+        None = 0,
+        Plural = 1,
+        ListSuffix = 2
+    }
+}
