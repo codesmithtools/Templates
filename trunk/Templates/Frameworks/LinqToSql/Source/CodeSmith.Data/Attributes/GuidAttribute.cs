dd2ee77deaba268af182d0dbdb5a524e607cc371
regbradii@gmail.com <regbradii@gmail.com@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2008-10-16 14:32:32 +0000

    Adding New Attributes to subversion

---
+++ trunk/Templates/Frameworks/LinqToSql/Source/CodeSmith.Data/Attributes/GuidAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using CodeSmith.Data.Rules;
+
+namespace CodeSmith.Data.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple= false)]
+    sealed public class GuidAttribute : ValidationAttribute
+    {
+        public GuidAttribute()
+        {
+            IsStateSet = false;
+        }
+
+        public GuidAttribute(EntityState state)
+        {
+            State = state;
+            IsStateSet = true;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value != null)
+                throw new ValidationException(ErrorMessage);
+            return true;
+        }
+        
+        public EntityState State { get; private set; }
+
+        public bool IsStateSet { get; private set; }
+
+        private string _errorMessage = "This field is automatically set.";
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set { _errorMessage = value; }
+        }
+    }
+}
