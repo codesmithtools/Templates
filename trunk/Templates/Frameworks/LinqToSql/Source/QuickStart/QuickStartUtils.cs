7d2aa8de1a4765b482e17108d242637371f62bf3
regbradii@gmail.com <regbradii@gmail.com@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2008-10-20 21:33:02 +0000

    QuickStart Files

---
+++ trunk/Templates/Frameworks/LinqToSql/Source/QuickStart/QuickStartUtils.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace QuickStartUtils
+{
+    public class QuickStartUtils
+    {
+        public enum LanguageEnum
+        {
+            CSharp = 1,
+            VB = 2
+        }
+
+        public enum ProjectTypeEnum
+        {
+            DynamicData = 1,
+            MVC = 2
+        }
+
+        public static void ReplaceAllInDirectory(string path, string find, string replace)
+        {
+            ReplaceAllInDirectory(path, find, replace, 0);
+        }
+
+        private static void ReplaceAllInDirectory(string path, string find, string replace, int level)
+        {
+            //Taken in part from http://weblogs.asp.net/israelio/archive/2004/06/23/162913.aspx
+            if (level <= 3)
+            {
+                // Process the list of files found in the directory.
+                string[] fileEntries = Directory.GetFiles(path);
+                foreach (string fileName in fileEntries)
+                {
+                   FindAndReplace(fileName, find, replace);
+                }
+
+                // Recurse into subdirectories of this directory.
+                string[] subdirEntries = Directory.GetDirectories(path);
+                foreach (string subdir in subdirEntries)
+                    // Do not iterate through reparse points
+                    if ((File.GetAttributes(subdir) & FileAttributes.ReparsePoint) != FileAttributes.ReparsePoint)
+                        ReplaceAllInDirectory(subdir,find,replace, level + 1);
+            }
+        }
+
+        public static void FindAndReplace(string fileName, string find, string replace)
+        {
+            //Taken From http://www.csharp411.com/searchreplace-in-files/
+            StreamReader reader = new StreamReader(fileName);
+            string content = reader.ReadToEnd();
+            reader.Close();
+
+            content = Regex.Replace(content, find, replace);
+
+            StreamWriter writer = new StreamWriter(fileName);
+            writer.Write(content);
+            writer.Close();
+        }
+
+        public static string FindFileInDirectory(string find, string path)
+        {
+            string[] fileEntries = Directory.GetFiles(path);
+            foreach (string fileName in fileEntries)
+            {
+                if (fileName.Contains(find))
+                    return fileName;
+            }
+            return null;
+        }
+
+        public static void CopyDirectory(string pathFrom, string pathTo, int level)
+        {
+            if (level <= 3)
+            {
+                if (!Directory.Exists(pathTo))
+                    Directory.CreateDirectory(pathTo);
+                string[] fileEntries = Directory.GetFiles(pathFrom);
+                foreach (string fileName in fileEntries)
+                {
+                    File.Copy(fileName,fileName.Replace(pathFrom,pathTo));
+                }
+
+                string[] subdirEntries = Directory.GetDirectories(pathFrom);
+                foreach (string subdir in subdirEntries)
+                    // Do not iterate through reparse points
+                    if ((File.GetAttributes(subdir) & FileAttributes.ReparsePoint) != FileAttributes.ReparsePoint)
+                        CopyDirectory(subdir, subdir.Replace(pathFrom,pathTo), level + 1);
+            }
+        }
+    }
+}
