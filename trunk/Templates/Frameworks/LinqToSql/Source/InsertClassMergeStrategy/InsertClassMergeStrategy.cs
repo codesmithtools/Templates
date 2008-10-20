880e6963f615ccda6e3d8fb830d796c734fd8a6b
tdupont750 <tdupont750@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2008-10-20 20:57:54 +0000

    Updated InsertClassMergeStrategy and made minor related fixes to Entities

--- trunk/Templates/Frameworks/LinqToSql/Source/InsertClassMergeStrategy/InsertClassMergeStrategy.cs
+++ trunk/Templates/Frameworks/LinqToSql/Source/InsertClassMergeStrategy/InsertClassMergeStrategy.cs
@@ -6,25 +6,34 @@ using System.Collections;
 using CodeSmith.Engine;
 using ICSharpCode.NRefactory;
 using ICSharpCode.NRefactory.Ast;
+using System.Text.RegularExpressions;
 
 namespace CodeSmith.Engine
 {
     public class InsertClassMergeStrategy : IMergeStrategy
     {
-        #region Declarations
-
-        private bool _preserveClassAttributes = false;
-
-        #endregion
+        public enum NotFoundActionEnum
+        {
+            None,
+            InsertAtBottom,
+            InsertInParent
+        }
 
         #region Constructors
 
         public InsertClassMergeStrategy() { }
-        public InsertClassMergeStrategy(string language, string className, bool preserveClassAttributes)
+        public InsertClassMergeStrategy(SupportedLanguage language, string className)
         {
             this.Language = language;
             this.ClassName = className;
+        }
+        public InsertClassMergeStrategy(SupportedLanguage language, string className, bool preserveClassAttributes, bool onlyInsertMatchingClass, NotFoundActionEnum notFoundAction, string notFoundParent)
+            : this(language, className)
+        {
             this.PreserveClassAttributes = preserveClassAttributes;
+            this.OnlyInsertMatchingClass = onlyInsertMatchingClass;
+            this.NotFoundAction = notFoundAction;
+            this.NotFoundParent = notFoundParent;
         }
 
         #endregion
@@ -34,12 +43,24 @@ namespace CodeSmith.Engine
         public void Init(string init)
         {
             Hashtable attribs = StringUtil.ParseConfigString(init);
+
             if (attribs.ContainsKey("Language"))
-                this.Language = (string)attribs["Language"];
+                SetLanguage((string)attribs["Language"]);
+
             if (attribs.ContainsKey("ClassName"))
-                this.Language = (string)attribs["ClassName"];
+                this.ClassName = (string)attribs["ClassName"];
+
             if (attribs.ContainsKey("PreserveClassAttributes"))
-                this.PreserveClassAttributes = Boolean.TryParse((string)attribs["PreserveClassAttributes"], out _preserveClassAttributes);
+                Boolean.TryParse((string)attribs["PreserveClassAttributes"], out _preserveClassAttributes);
+
+            if (attribs.ContainsKey("OnlyInsertMatchingClass"))
+                Boolean.TryParse((string)attribs["OnlyInsertMatchingClass"], out _onlyInsertMatchingClass);
+
+            if (attribs.ContainsKey("NotFoundAction"))
+                this.NotFoundAction = (NotFoundActionEnum)Enum.Parse(typeof(NotFoundActionEnum), (string)attribs["NotFoundAction"]);
+
+            if (attribs.ContainsKey("NotFoundParent"))
+                this.NotFoundParent = (string)attribs["NotFoundParent"];
         }
         public string Merge(CodeTemplate context, string sourceContent, string templateOutput)
         {
@@ -50,40 +71,83 @@ namespace CodeSmith.Engine
             // Check Merge Settings
             if (String.IsNullOrEmpty(this.ClassName))
                 throw new Exception("No ClassName specified for InsertClassMerge.");
-            if (String.IsNullOrEmpty(Language) || Language.Trim().Length == 0)
-                Language = context.CodeTemplateInfo.TargetLanguage;
+            if (!this.Language.HasValue && !SetLanguage(context.CodeTemplateInfo.TargetLanguage))
+                throw new Exception("InsertClassMergeStrategy only supports Languages 'C#' and 'VB'");
 
             // Parse Source
-            CodeFileParser sourceParser = CodeFileParser.Create(sourceContent, this.supportedLanguage);
+            CodeFileParser sourceParser = CodeFileParser.Create(sourceContent, this.Language.Value);
             sourceParser.ParseMethodBodies = false;
             sourceParser.Parse();
             AttributeSectionVisitor sourceVisitor = new AttributeSectionVisitor();
             sourceParser.CompilationUnit.AcceptVisitor(sourceVisitor, this.ClassName);
 
-            // Parse Template
-            CodeFileParser templateParser = CodeFileParser.Create(templateOutput, this.supportedLanguage);
-            templateParser.ParseMethodBodies = false;
-            templateParser.Parse();
-            AttributeSectionVisitor templateVisitor = new AttributeSectionVisitor();
-            templateParser.CompilationUnit.AcceptVisitor(templateVisitor, this.ClassName);
+            // Check OnlyInsertMatchingClass
+            if (OnlyInsertMatchingClass)
+            {
+                // Parse Template
+                CodeFileParser templateParser = CodeFileParser.Create(templateOutput, this.Language.Value);
+                templateParser.ParseMethodBodies = false;
+                templateParser.Parse();
+                AttributeSectionVisitor templateVisitor = new AttributeSectionVisitor();
+                templateParser.CompilationUnit.AcceptVisitor(templateVisitor, this.ClassName);
 
-            // Exit it classes to merge are not found.
-            if (sourceVisitor.Type == null || templateVisitor.Type == null)
-                return sourceContent;
+                // If no class found (nothing to merge), return the source.
+                if (templateVisitor.Type == null)
+                    return sourceContent;
 
-            // Setup For Merge
+                // Update TemplateOutput (Trim everything but the existing class!)
+                templateOutput = templateParser.GetSection(
+                    GetCorrectedTypeStart(templateVisitor.Type),
+                    GetCorrectedTypeEnd(templateVisitor.Type));
+            }
+
+            // Setup Variables For Merge
             StringBuilder mergeResult = new StringBuilder();
+            Location sourceStart;
+            Location sourceEnd;
+
+            // Set Source Start & End Locations
+            if (sourceVisitor.Type == null)
+            {
+                switch (NotFoundAction)
+                {
+                    case NotFoundActionEnum.InsertAtBottom:
+                        // Set templateStart & templateEnd as bottom.
+                        sourceStart = sourceParser.SourceEnd;
+                        sourceEnd = sourceParser.SourceEnd;
+                        break;
+
+                    case NotFoundActionEnum.InsertInParent:
+                        // Insert class before end of parent section.
+                        sourceStart = GetBeforeEnd(sourceParser);
+                        sourceEnd = sourceStart;
+                        // Try to fix up our output...
+                        Regex findWhiteSpace = new Regex(@"^(\s)*");
+                        string whiteSpace = findWhiteSpace.Match(sourceParser.SourceContents[sourceStart.Line - 1]).Value;
+                        templateOutput = String.Concat(templateOutput, Environment.NewLine, whiteSpace);
+                        break;
+
+                    case NotFoundActionEnum.None:
+                    default:
+                        // Nothing to merge, return the source.
+                        return sourceContent;
+                }
+            }
+            else
+            {
+                // Class exists, set it's start and end.
+                sourceStart = GetCorrectedTypeStart(sourceVisitor.Type);
+                sourceEnd = GetCorrectedTypeEnd(sourceVisitor.Type);
+            }
 
             // Add Pre-Class Text To Output
-            mergeResult.Append(sourceParser.GetSectionFromStart(CorrectedTypeStart(sourceVisitor.Type)));
+            mergeResult.Append(sourceParser.GetSectionFromStart(sourceStart));
 
             // Add Merged Class to Output
-            mergeResult.Append(templateParser.GetSection(
-                CorrectedTypeStart(templateVisitor.Type),
-                templateVisitor.Type.EndLocation));
+            mergeResult.Append(templateOutput);
 
             // Add Post-Class Text To Output
-            mergeResult.Append(sourceParser.GetSectionToEnd(sourceVisitor.Type.EndLocation));
+            mergeResult.Append(sourceParser.GetSectionToEnd(sourceEnd));
 
             // Return MergeResult
             return mergeResult.ToString();
@@ -93,11 +157,54 @@ namespace CodeSmith.Engine
 
         #region Methods
 
-        protected Location CorrectedTypeStart(TypeDeclaration typeDeclaration)
+        protected Location GetCorrectedTypeStart(TypeDeclaration type)
         {
-            return (!PreserveClassAttributes && typeDeclaration.Attributes.Count > 0)
-                ? typeDeclaration.Attributes.First().StartLocation
-                : typeDeclaration.StartLocation;
+            return (!PreserveClassAttributes && type.Attributes.Count > 0)
+                ? type.Attributes.First().StartLocation
+                : type.StartLocation;
+        }
+        protected Location GetCorrectedTypeEnd(TypeDeclaration type)
+        {
+            Location result = new Location(type.EndLocation.Column, type.EndLocation.Line);
+            if (this.Language.Value == SupportedLanguage.CSharp)
+                result.Column++;
+            return result;
+        }
+
+        protected Location GetBeforeEnd(CodeFileParser parser)
+        {
+            ParentVisitor parentVisitor = new ParentVisitor();
+            parser.CompilationUnit.AcceptVisitor(parentVisitor, NotFoundParent);
+
+            if(parentVisitor.Node == null)
+                throw new Exception(String.Format("Can not find Namespace or Class with name {0}.", NotFoundParent));
+
+            if (parentVisitor.Node is NamespaceDeclaration)
+            {
+                return (Language.Value == SupportedLanguage.CSharp)
+                    ? new Location(parentVisitor.Node.EndLocation.Column - 1, parentVisitor.Node.EndLocation.Line)  // }
+                    : new Location(parentVisitor.Node.EndLocation.Column - 13, parentVisitor.Node.EndLocation.Line);// End Namespace
+            }
+            else if (parentVisitor.Node is TypeDeclaration)
+            {
+                return (Language.Value == SupportedLanguage.CSharp)
+                    ? new Location(parentVisitor.Node.EndLocation.Column - 0, parentVisitor.Node.EndLocation.Line)  // BUG: The C# Type Delcaration ends early?
+                    : new Location(parentVisitor.Node.EndLocation.Column - 9, parentVisitor.Node.EndLocation.Line); // End Class
+            }
+            else
+                throw new Exception(String.Format("ParentVisitor should not return {0}", parentVisitor.Node.GetType().Name));
+        }
+
+        protected bool SetLanguage(string language)
+        {
+            if (String.Compare(language, "C#", true) == 0 || String.Compare(language, "CSharp", true) == 0)
+                Language = SupportedLanguage.CSharp;
+            else if (String.Compare(language, "VB", true) == 0 || String.Compare(language, "VBNet", true) == 0)
+                Language = SupportedLanguage.VBNet;
+            else
+                return false;
+
+            return true;
         }
 
         #endregion
@@ -105,24 +212,34 @@ namespace CodeSmith.Engine
         #region Properties
 
         public string ClassName { get; set; }
-        public string Language { get; set; }
+        public string NotFoundParent { get; set; }
+
+        private bool _onlyInsertMatchingClass = false;
+        public bool OnlyInsertMatchingClass
+        {
+            get { return _onlyInsertMatchingClass; }
+            set { _onlyInsertMatchingClass = value; }
+        }
+
+        private bool _preserveClassAttributes = false;
         public bool PreserveClassAttributes
         {
             get { return _preserveClassAttributes; }
             set { _preserveClassAttributes = value; }
         }
 
-        protected SupportedLanguage supportedLanguage
+        private NotFoundActionEnum _notFoundAction = NotFoundActionEnum.None;
+        public NotFoundActionEnum NotFoundAction
         {
-            get
-            {
-                if (String.Compare(Language, "C#", true) == 0)
-                    return SupportedLanguage.CSharp;
-                else if (String.Compare(Language, "VB", true) == 0)
-                    return SupportedLanguage.VBNet;
-                else
-                    throw new Exception(String.Format("{0} is not a supported language for the ClassMerge.", Language));
-            }
+            get { return _notFoundAction; }
+            set { _notFoundAction = value; }
+        }
+
+        private SupportedLanguage? _language = null;
+        public SupportedLanguage? Language
+        {
+            get { return _language; }
+            set { _language = value; }
         }
 
         #endregion
---
+++ trunk/Templates/Frameworks/LinqToSql/Source/InsertClassMergeStrategy/ParentVisitor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ICSharpCode.NRefactory.Visitors;
+using ICSharpCode.NRefactory.Ast;
+
+namespace CodeSmith.Engine
+{
+    internal class ParentVisitor : AbstractAstVisitor
+    {
+        #region AbstractAstVisitor Overrides
+
+        public override object VisitNamespaceDeclaration(NamespaceDeclaration namespaceDeclaration, object data)
+        {
+            if (Node == null && namespaceDeclaration.Name.Equals(data))
+            {
+                Node = namespaceDeclaration;
+            }
+
+            return base.VisitNamespaceDeclaration(namespaceDeclaration, data);
+        }
+        public override object VisitTypeDeclaration(TypeDeclaration typeDeclaration, object data)
+        {
+            if (Node == null && typeDeclaration.Name.Equals(data))
+            {
+                Node = typeDeclaration;
+            }
+
+            return base.VisitTypeDeclaration(typeDeclaration, data);
+        }
+
+        #endregion
+
+        public INode Node { get; private set; }
+    }
+}
