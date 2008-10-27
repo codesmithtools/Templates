487298677dd625238bd71a0085e5464ab5da1b80
regbradii@gmail.com <regbradii@gmail.com@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2008-10-27 18:39:25 +0000

    File paths now relative to cst for copy can set exempt direcotires for bulk find and replace, above used to fix Image problem

--- trunk/Templates/Frameworks/LinqToSql/Source/QuickStart/QuickStartUtils.cs
+++ trunk/Templates/Frameworks/LinqToSql/Source/QuickStart/QuickStartUtils.cs
@@ -21,32 +21,44 @@ namespace QuickStartUtils
             MVC = 2
         }
 
-        public static void ReplaceAllInDirectory(string path, string find, string replace)
+        public static void ReplaceAllInDirectory(string path, string find, string replace, string[] exemptDirectories)
         {
-            ReplaceAllInDirectory(path, find, replace, 0);
+            ReplaceAllInDirectory(path, find, replace,exemptDirectories, 0);
         }
 
-        private static void ReplaceAllInDirectory(string path, string find, string replace, int level)
+        public static void ReplaceAllInDirectory(string path, string find, string replace)
+        {
+            ReplaceAllInDirectory(path, find, replace, null ,0);
+        }
+
+        private static void ReplaceAllInDirectory(string path, string find, string replace,string[] exemptDirectories, int level)
         {
             //Taken in part from http://weblogs.asp.net/israelio/archive/2004/06/23/162913.aspx
             if (level <= 3)
             {
+                Console.Out.WriteLine(path);
                 // Process the list of files found in the directory.
                 string[] fileEntries = Directory.GetFiles(path);
                 foreach (string fileName in fileEntries)
                 {
-                   FindAndReplace(fileName, find, replace);
+                    FindAndReplace(fileName, find, replace);
                 }
 
                 // Recurse into subdirectories of this directory.
                 string[] subdirEntries = Directory.GetDirectories(path);
                 foreach (string subdir in subdirEntries)
+                {
                     // Do not iterate through reparse points
                     if ((File.GetAttributes(subdir) & FileAttributes.ReparsePoint) != FileAttributes.ReparsePoint)
-                        ReplaceAllInDirectory(subdir,find,replace, level + 1);
+                    {
+                        if (exemptDirectories != null && exemptDirectories.Count<string>(d => subdir.EndsWith(d)) == 0)
+                            ReplaceAllInDirectory(subdir, find, replace, exemptDirectories, level + 1);
+                    }
+                    
+                }
             }
         }
-
+        
         public static void FindAndReplace(string fileName, string find, string replace)
         {
             //Taken From http://www.csharp411.com/searchreplace-in-files/
@@ -96,7 +108,30 @@ namespace QuickStartUtils
                 foreach (string subdir in subdirEntries)
                     // Do not iterate through reparse points
                     if ((File.GetAttributes(subdir) & FileAttributes.ReparsePoint) != FileAttributes.ReparsePoint)
-                        CopyDirectory(subdir, subdir.Replace(pathFrom,pathTo), level + 1);
+                        CopyDirectory(subdir, subdir.Replace(pathFrom,pathTo + @"\"), level + 1);
+            }
+        }
+
+        public static void ReplaceAllFilesInDirectory(string directoryFind, string directoryReplace)
+        {
+            ReplaceAllFilesInDirectory(directoryFind, directoryReplace, String.Empty);
+        }
+
+        public static void ReplaceAllFilesInDirectory(string directoryFind, string directoryReplace, string fileExtension)
+        {
+            if(fileExtension != String.Empty)
+            {
+                foreach(string file in Directory.GetFiles(directoryFind,fileExtension))
+                {
+                    File.Copy(file.Replace(directoryFind, directoryReplace), file, false);
+                }
+            }
+            else
+            {
+                foreach(string file in Directory.GetFiles(directoryFind))
+                {
+                    File.Copy(file.Replace(directoryFind, directoryReplace), file, false);
+                }
             }
         }
     }
