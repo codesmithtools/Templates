86d2515e6f9c4bb0b6c3fb49015a553ca36d0e3b
Eric J. Smith <eric@codesmithtools.com>
2008-11-17 22:34:59 +0000

    Changed the AddShared method to look for RuleAttribute and call the CreateRule method so that new attributes can be created without modifying the CodeSmith.Data assembly.

--- Templates/Frameworks/LinqToSql/Source/CodeSmith.Data/Attributes/NowAttribute.cs
+++ Templates/Frameworks/LinqToSql/Source/CodeSmith.Data/Attributes/NowAttribute.cs
@@ -1,42 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
 using CodeSmith.Data.Rules;
+using CodeSmith.Data.Rules.Assign;
 
 namespace CodeSmith.Data.Attributes
 {
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple= false)]
-    sealed public class NowAttribute : ValidationAttribute
+    /// <summary>
+    /// Assigns the current date / time to the property for the specified entity states.
+    /// </summary>
+    public class NowAttribute : RuleAttribute
     {
-
         public NowAttribute()
         {
-            IsStateSet = false;
         }
 
         public NowAttribute(EntityState state)
         {
-            State = state;
-            IsStateSet = true;
+            this.State = state;
+        }
+
+        public override IRule CreateRule(PropertyInfo property)
+        {
+            return new NowRule(property.Name, this.State);
         }
 
         public override bool IsValid(object value)
         {
             return true;
-        }
-        
-        public EntityState State { get; private set; }
-
-        public bool IsStateSet { get; private set; }
-
-        private string _errorMessage = "This field is automatically set.";
-
-        public string ErrorMessage
-        {
-            get { return _errorMessage; }
-            set { _errorMessage = value; }
         }
     }
 }
---
+++ Templates/Frameworks/LinqToSql/Source/CodeSmith.Data/Attributes/RuleAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using CodeSmith.Data.Rules;
+
+namespace CodeSmith.Data.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public abstract class RuleAttribute : ValidationAttribute
+    {
+        protected RuleAttribute()
+        {
+            this.State = EntityState.All;
+        }
+        
+        public abstract IRule CreateRule(PropertyInfo property);
+
+        public EntityState State { get; protected set; }
+    }
+}
