a6f74961e66854a8debec99cc6c0c7f3d9bf6b00
tdupont750 <tdupont750@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2008-11-22 00:27:53 +0000

    Added Enum support to C# Plinqo.

--- trunk/Templates/Frameworks/LinqToSql/Source/Generator/GeneratorSettings.cs
+++ trunk/Templates/Frameworks/LinqToSql/Source/Generator/GeneratorSettings.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using SchemaExplorer;
 
 namespace LinqToSqlShared.Generator
 {
@@ -59,6 +60,30 @@ namespace LinqToSqlShared.Generator
             get { return _cleanExpressions; }
         }
 
+        private List<Regex> _enumExpressions = new List<Regex>();
+        public List<Regex> EnumExpressions
+        {
+            get { return _enumExpressions; }
+        }
+
+        private List<Regex> _enumNameExpressions = new List<Regex>();
+        public List<Regex> EnumNameExpressions
+        {
+            get { return _enumNameExpressions; }
+        }
+
+        private List<Regex> _enumDescriptionExpressions = new List<Regex>();
+        public List<Regex> EnumDescriptionExpressions
+        {
+            get { return _enumDescriptionExpressions; }
+        }
+
+        private List<Regex> _enumSummaryExpressions = new List<Regex>();
+        public List<Regex> EnumSummaryExpressions
+        {
+            get { return _enumSummaryExpressions; }
+        }
+
         private bool _disableRenaming = false;
         public bool DisableRenaming
         {
@@ -68,16 +93,60 @@ namespace LinqToSqlShared.Generator
 
         public bool IsIgnored(string name)
         {
-            if (IgnoreExpressions.Count == 0)
-                return false;
+            return IsRegexMatch(name, IgnoreExpressions);
+        }
 
-            foreach (Regex regex in IgnoreExpressions)
-            {
+        public bool IsEnum(TableSchema table)
+        {
+            return !IsIgnored(table.Name)                                       // 1) Is not ignored.
+                && IsRegexMatch(table.Name, EnumExpressions)                    // 2) Matches the enum regex.
+                && table.PrimaryKey != null                                     // 3) Has a Primary Key...
+                && table.PrimaryKey.MemberColumns.Count == 1                    // 4) ...that is a single column...
+                && table.PrimaryKey.MemberColumns[0].SystemType == typeof(int)  // 5) ...of type integer.
+                && !string.IsNullOrEmpty(GetEnumNameColumnName(table));         // 6) Contains a column for name.
+        }
+
+        public string GetEnumNameColumnName(TableSchema table)
+        {
+            return GetEnumColumnName(table, EnumNameExpressions);
+        }
+
+        public string GetEnumSummaryColumnName(TableSchema table)
+        {
+            return GetEnumColumnName(table, EnumSummaryExpressions);
+        }
+
+        public string GetEnumDescriptionColumnName(TableSchema table)
+        {
+            return GetEnumColumnName(table, EnumDescriptionExpressions);
+        }
+
+        public string GetEnumColumnName(TableSchema table, List<Regex> regexList)
+        {
+            string result = string.Empty;
+
+            foreach (ColumnSchema column in table.Columns)
+                if (IsRegexMatch(column.Name, regexList))
+                {
+                    result = column.Name;
+                    break;
+                }
+
+            return result;
+        }
+
+        private bool IsRegexMatch(string name, List<Regex> regexList)
+        {
+            bool isEnum = false;
+
+            foreach (Regex regex in regexList)
                 if (regex.IsMatch(name))
-                    return true;
-            }
+                {
+                    isEnum = true;
+                    break;
+                }
 
-            return false;
+            return isEnum;
         }
 
         public string CleanName(string name)
