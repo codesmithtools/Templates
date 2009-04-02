727021f865441f80967b03f5a6daa4397285f6a3
Blake Niemyjski <bniemyjski@gmail.com>
2009-04-02 21:29:29 +0000

    Added Clean Entity Names Expression and IgnoreTables Expression

--- Templates/Frameworks/CodeSmith.CSLA/Source/QuickStart/Base/CSLAMasterTemplate.cs
+++ Templates/Frameworks/CodeSmith.CSLA/Source/QuickStart/Base/CSLAMasterTemplate.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Text.RegularExpressions;
 
+using CodeSmith.CustomProperties;
 using CodeSmith.Engine;
 using CodeSmith.SchemaHelper;
 
 using SchemaExplorer;
+
+using Configuration=CodeSmith.SchemaHelper.Configuration;
 
 namespace QuickStart
 {
@@ -22,6 +26,8 @@ namespace QuickStart
 
         public CSLAMasterTemplate()
         {
+            CleanExpressions = new StringCollection();
+            IgnoreExpressions = new StringCollection();
         }
 
         #endregion
@@ -46,6 +52,18 @@ namespace QuickStart
             }
         }
 
+        [Category("1. DataSource")]
+        [Description("List of regular expressions to clean table, view and column names.")]
+        [Optional]
+        [DefaultValue("^\\w+_")]
+        public CodeSmith.CustomProperties.StringCollection CleanExpressions { get; set; }
+
+        [Category("1. DataSource")]
+        [Description("List of regular expressions to ignore tables when generating.")]
+        [Optional]
+        [DefaultValue("sysdiagrams$")]
+        public CodeSmith.CustomProperties.StringCollection IgnoreExpressions { get; set; }
+
         [Browsable(false)]
         public List<Entity> Entities { get; internal set; }
 
@@ -62,6 +80,30 @@ namespace QuickStart
 
         public virtual void OnTablesChanged()
         {
+            if (CleanExpressions.Count == 0)
+                CleanExpressions.Add("^\\w+_");
+
+            if (IgnoreExpressions.Count == 0)
+                IgnoreExpressions.Add("sysdiagrams$");
+
+            Configuration.Instance.CleanExpressions = new List<Regex>();
+            foreach (string clean in CleanExpressions)
+            {
+                if (!string.IsNullOrEmpty(clean))
+                {
+                    Configuration.Instance.CleanExpressions.Add(new Regex(clean));
+                }
+            }
+
+            Configuration.Instance.IgnoreExpressions = new List<Regex>();
+            foreach (string ignore in IgnoreExpressions)
+            {
+                if (!string.IsNullOrEmpty(ignore))
+                {
+                    Configuration.Instance.IgnoreExpressions.Add(new Regex(ignore));
+                }
+            }
+
             Entities = new EntityManager(SourceTables).Entities;
 
             if (string.IsNullOrEmpty(Location) && SourceTables.Count > 0)
