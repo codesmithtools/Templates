fe13009e38a2d6f4833b394039d1cb42f214072c
Paul Welter <pwelter@loresoft.com>
2009-05-28 18:29:07 +0000

    - add shared version information for Assemblies - change assembly version to 3.0.0.0

--- Templates/Frameworks/PLINQO/Source/CodeSmith.Data/Properties/AssemblyInfo.cs
+++ Templates/Frameworks/PLINQO/Source/CodeSmith.Data/Properties/AssemblyInfo.cs
@@ -4,37 +4,12 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Web.UI;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("CodeSmith.Data")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CodeSmith Tools")]
-[assembly: AssemblyProduct("CodeSmith.Data")]
-[assembly: AssemblyCopyright("Copyright (c) 2002-2008 CodeSmith Tools, LLC.  All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("626628c2-ee13-4979-9ab2-6b200f7928fd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: TagPrefix("CodeSmith.Data.Web", "cs")]
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("CodeSmith.Data.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001007DEAE93F0140255A0E173CBBC9DC3859D18A867B5A96290111ADD148457BF1E16DE12BD7DF16D509B9E0239B1D28F26287C5120D48A3A5E967D0E220B586ECCC502F694659CE8395C008AEA18188F9E59A7F5961E84E754E4A5CD0A57E8DDE5A86AE3747F6A3745C2BF6937CCEAF59F1CBD2444557C81B8E13AFD69B2636B4BE")]
