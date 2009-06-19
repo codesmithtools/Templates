b6ccde59b3bf8279995fcc295721b94319800749
bniemyjski <bniemyjski@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2009-06-19 16:12:11 +0000

    Tagged CSLA 1.0

---
+++ tags/CSLA-1.0/Source/CodeSmith.SchemaHelper/Member.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using SchemaExplorer;
+
+namespace CodeSmith.SchemaHelper
+{
+    public class Member : MemberBase
+    {
+        #region Constructor(s)
+
+        public Member(ColumnSchema column) : this(column, null)
+        {
+            IsForeignKey = false;
+        }
+        
+        public Member(ColumnSchema column, Entity entity) : base(column, entity)
+        {
+            IsForeignKey = false;
+        }
+
+        public Member(ColumnSchema column, Entity entity, bool isForeignKey) : base(column, entity)
+        {
+            IsForeignKey = isForeignKey;
+        }
+
+        #endregion
+
+        #region Public Read-Only Variables
+
+        public bool IsForeignKey { get; private set; }
+
+        public bool IsReadOnly
+        {
+            get { return (IsIdentity || IsRowVersion || IsComputed); }
+        }
+
+        #endregion
+    }
+}
