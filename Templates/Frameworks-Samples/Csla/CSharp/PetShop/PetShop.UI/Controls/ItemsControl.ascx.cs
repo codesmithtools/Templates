db13b5e41862142bd3bb85c1346c30b64e28abb1
Blake Niemyjski <bniemyjski@gmail.com>
2009-06-30 19:16:12 +0000

    moved to petshop folder (oops)

---
+++ Templates/Frameworks-Samples/Csla/CSharp/PetShop/PetShop.UI/Controls/ItemsControl.ascx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using PetShop.Business;
+
+namespace PetShop.UI.Controls
+{
+    public partial class ItemsControl : UserControl
+    {
+        /// <summary>
+        /// Rebind control 
+        /// </summary>
+        protected void PageChanged(object sender, DataGridPageChangedEventArgs e)
+        {
+            //reset index
+            itemsGrid.CurrentPageIndex = e.NewPageIndex;
+
+            //get category id
+            string productId = Request.QueryString["productId"];
+
+            itemsGrid.DataSource = Product.GetProduct(productId).Items;
+            itemsGrid.DataBind();
+        }
+    }
+}
