c72472c03c5f418e539f34db7facbac6dc750cd1
shannon.davidson <shannon.davidson@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2009-07-15 21:55:03 +0000

    Reorganizing the Tracker project.

---
+++ trunk/Projects/Framework-Samples/PLINQO/CSharp/Tracker/Source/Tracker.Core/Data/Entities/Task.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Linq;
+using System.Data.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using CodeSmith.Data.Attributes;
+using CodeSmith.Data.Rules;
+using Tracker.Core.Data.Rules;
+using Tracker.Core.Data.Rules.Attributes;
+
+namespace Tracker.Core.Data
+{
+    public partial class Task
+    {
+        // For more information about the features contained in this class, please visit our GoogleCode Wiki at...
+        // http://code.google.com/p/codesmith/wiki/PLINQO
+        // Also, you can watch our Video Tutorials at...
+        // http://community.codesmithtools.com/
+
+        static partial void AddSharedRules()
+        {
+            RuleManager.AddShared<Task>(new TaskAssignmentRule());
+        }
+
+        #region Metadata
+
+        [CodeSmith.Data.Audit.Audit]
+        internal class Metadata
+        {
+            // Only Attributes in the class will be preserved.
+
+            public int Id { get; set; }
+
+            [Required(ErrorMessage="Status is Required")]
+            public int StatusId { get; set; }
+
+            public Priority PriorityId { get; set; }
+
+            [CurrentUserName(EntityState.New)]
+            public int CreatedId { get; set; }
+
+            [Required(ErrorMessage = "Summary is Required")]
+            public string Summary { get; set; }
+
+            [DataType(System.ComponentModel.DataAnnotations.DataType.MultilineText)]
+            public string Details { get; set; }
+
+            public System.DateTime StartDate { get; set; }
+
+            public System.DateTime DueDate { get; set; }
+
+            public System.DateTime CompleteDate { get; set; }
+
+            public int AssignedId { get; set; }
+
+            [Now(EntityState.New)]
+            [CodeSmith.Data.Audit.NotAudited]
+            public System.DateTime CreatedDate { get; set; }
+
+            [Now(EntityState.Dirty)]
+            [CodeSmith.Data.Audit.NotAudited]
+            public System.DateTime ModifiedDate { get; set; }
+
+            public System.Data.Linq.Binary RowVersion { get; set; }
+
+            [UserName(EntityState.Dirty)]
+            [CodeSmith.Data.Audit.NotAudited]
+            public string LastModifiedBy { get; set; }
+
+            public Status Status { get; set; }
+
+            public User AssignedUser { get; set; }
+
+            public User CreatedUser { get; set; }
+
+            public TaskExtended TaskExtended { get; set; }
+
+            public EntitySet<Audit> AuditList { get; set; }
+
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/Projects/Framework-Samples/PLINQO/CSharp/Tracker/Source/Tracker.Core/Data/Entities/User.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Data.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using CodeSmith.Data.Attributes;
+using CodeSmith.Data.Rules;
+using CodeSmith.Data.Rules.Validation;
+using Tracker.Core.Data.Rules;
+
+namespace Tracker.Core.Data
+{
+    public partial class User
+    {
+        // For more information about the features contained in this class, please visit our GoogleCode Wiki at...
+        // http://code.google.com/p/codesmith/wiki/PLINQO
+        // Also, you can watch our Video Tutorials at...
+        // http://community.codesmithtools.com/
+
+        public string FullName
+        {
+            get { return String.Concat(this.FirstName, String.IsNullOrEmpty(this.FirstName) ? String.Empty : " ", this.LastName).Trim(); }
+        }
+
+        static partial void AddSharedRules()
+        {
+            CodeSmith.Data.Rules.RuleManager.AddShared<User>(new UserExistsRule());
+        }
+
+        #region Metadata
+
+        [CodeSmith.Data.Audit.Audit]
+        internal class Metadata
+        {
+            // Only Attributes in the class will be preserved.
+
+            public int Id { get; set; }
+
+            [DataType(System.ComponentModel.DataAnnotations.DataType.EmailAddress)]
+            public string EmailAddress { get; set; }
+
+            [Required]
+            public string FirstName { get; set; }
+
+            [Required]
+            public string LastName { get; set; }
+
+            public System.Data.Linq.Binary Avatar { get; set; }
+
+            [Now(EntityState.New)]
+            [CodeSmith.Data.Audit.NotAudited]
+            public System.DateTime CreatedDate { get; set; }
+
+            [Now(EntityState.Dirty)]
+            [CodeSmith.Data.Audit.NotAudited]
+            public System.DateTime ModifiedDate { get; set; }
+
+            public System.Data.Linq.Binary RowVersion { get; set; }
+
+            [Required]
+            [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+            public string PasswordHash { get; set; }
+
+            [Required]
+            [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+            public string PasswordSalt { get; set; }
+
+            public string Comment { get; set; }
+
+            public bool IsApproved { get; set; }
+
+            public System.DateTime LastLoginDate { get; set; }
+
+            [CodeSmith.Data.Audit.NotAudited]
+            [Now(EntityState.Dirty)]
+            public System.DateTime LastActivityDate { get; set; }
+
+            public System.DateTime LastPasswordChangeDate { get; set; }
+
+            public string AvatarType { get; set; }
+
+            public EntitySet<Task> AssignedTaskList { get; set; }
+
+            public EntitySet<Task> CreatedTaskList { get; set; }
+
+            public EntitySet<UserRole> UserRoleList { get; set; }
+
+            public EntitySet<Audit> AuditList { get; set; }
+
+        }
+
+        #endregion
+    }
+}
