8b3c812dd239635f61d153b5bdd09663b7ca1c33
Blake Niemyjski <bniemyjski@gmail.com>
2009-07-27 19:02:50 +0000

    Updated Merge samples.

--- Templates/Examples/Merge/VisualBasic/PreserveRegionsSample.vb
+++ Templates/Examples/Merge/VisualBasic/PreserveRegionsSample.vb
@@ -1,11 +1,15 @@
 Public Class PreserveRegionsSample
 
 #Region "Custom Region 1"
-
-    ' This is my custom code that I want to preserve.
-    ' I can make changes to it and my changes will
-    ' not be overwritten.
-
+	
+	' This is a place holder for your custom code.
+	' It must exist so that CodeSmith knows where
+	' to put the custom code that will be parsed
+	' from the target source file.
+	' The region name is used to match up the regions
+	' and determine where each region of custom code
+	' should be inserted into the merge result.
+	
 #End Region
 
 
@@ -14,14 +18,14 @@ Public Class PreserveRegionsSample
         ' This section and all other non-custom code
         ' regions will be overwritten during each
         ' template execution.
-        ' Current Date: 11/3/2008 9:24:28 AM
+        ' Current Date: 7/27/2009 2:02:07 PM
 
     End Sub
 
 
 #Region "Custom Region 2"
 
-    ' The contents of this region will also be preserved
+    ' The contents will also be preserved
     ' during generation.
 
 #End Region
