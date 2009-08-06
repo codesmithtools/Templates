f1b113f75986fec3c6babcaab91d43acd8ae8431
Eric J. Smith <eric@codesmithtools.com>
2009-08-06 04:48:24 +0000

    Added allow partially trusted attribute to the CodeSmith.Data project to allow running in medium trust.

--- Templates/Frameworks/PLINQO/Source/CodeSmith.Data/Properties/AssemblyInfo.cs
+++ Templates/Frameworks/PLINQO/Source/CodeSmith.Data/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 using System.Web.UI;
 
 [assembly: AssemblyTitle("CodeSmith.Data")]
@@ -13,3 +14,4 @@ using System.Web.UI;
 [assembly: TagPrefix("CodeSmith.Data.Web", "cs")]
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("CodeSmith.Data.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001007DEAE93F0140255A0E173CBBC9DC3859D18A867B5A96290111ADD148457BF1E16DE12BD7DF16D509B9E0239B1D28F26287C5120D48A3A5E967D0E220B586ECCC502F694659CE8395C008AEA18188F9E59A7F5961E84E754E4A5CD0A57E8DDE5A86AE3747F6A3745C2BF6937CCEAF59F1CBD2444557C81B8E13AFD69B2636B4BE")]
+[assembly: AllowPartiallyTrustedCallers] 
