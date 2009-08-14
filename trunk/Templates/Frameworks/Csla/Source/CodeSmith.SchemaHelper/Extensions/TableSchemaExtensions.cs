7bc052412de8d05548ce785697032993a2e9e10c
tdupont750@gmail.com <tdupont750@gmail.com@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2009-08-14 19:21:17 +0000

    Updated many to many extended property.

--- trunk/Templates/Frameworks/Csla/Source/CodeSmith.SchemaHelper/Extensions/TableSchemaExtensions.cs
+++ trunk/Templates/Frameworks/Csla/Source/CodeSmith.SchemaHelper/Extensions/TableSchemaExtensions.cs
@@ -95,7 +95,11 @@ namespace CodeSmith.SchemaHelper
         {
             // Bypass logic if table contains Extended Property for ManyToMany
             if (table.ExtendedProperties.Contains(Configuration.Instance.ManyToManyExtendedProperty))
-                return true;
+            {
+                bool manyToMany;
+                if (Boolean.TryParse(table.ExtendedProperties[Configuration.Instance.ManyToManyExtendedProperty].Value.ToString(), out manyToMany))
+                    return manyToMany;
+            }
 
             // 1) Table must have Two ForeignKeys.
             // 2) All columns must be either...
