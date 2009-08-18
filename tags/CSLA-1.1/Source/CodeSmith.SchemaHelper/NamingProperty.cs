e50b01f8f3a5d5861c12e0d680fbd217e58e2cec
bniemyjski <bniemyjski@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2009-08-18 18:01:52 +0000

    CSLA 1.1 tag

---
+++ tags/CSLA-1.1/Source/CodeSmith.SchemaHelper/NamingProperty.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+using CodeSmith.Engine;
+
+using System.ComponentModel;
+
+namespace CodeSmith.SchemaHelper
+{
+    [Serializable]
+    [PropertySerializer(typeof(XmlPropertySerializer))]
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public class NamingProperty
+    {
+        #region Private Member(s)
+
+        private TableNaming _tableNaming = SchemaHelper.TableNaming.Singular;
+        private EntityNaming _entityNaming = SchemaHelper.EntityNaming.Singular;
+        private AssociationNaming _associationNaming = SchemaHelper.AssociationNaming.Table;
+        private AssociationSuffix _associationSuffix = SchemaHelper.AssociationSuffix.Plural;
+
+        #endregion
+
+        #region Constructor(s)
+
+        public NamingProperty()
+        {
+        }
+
+        #endregion
+
+        #region Public Overridden Method(s)
+
+        public override string ToString()
+        {
+            return "(Expand to edit...)";
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        [NotifyParentProperty(true)]
+        [Description("Table naming convention used in the database.")]
+        public TableNaming TableNaming
+        {
+            get { return _tableNaming; }
+            set { _tableNaming = value; }
+        }
+
+        [NotifyParentProperty(true)]
+        [Description("Desired naming naming convention to be used by generator.")]
+        public EntityNaming EntityNaming
+        {
+            get { return _entityNaming; }
+            set { _entityNaming = value; }
+        }
+
+        [NotifyParentProperty(true)]
+        [Description("Desired association naming convention to be used by generator.")]
+        public AssociationNaming AssociationNaming
+        {
+            get { return _associationNaming; }
+            set { _associationNaming = value; }
+        }
+
+        [NotifyParentProperty(true)]
+        [Description("Desired association suffix naming convention to be used by generator.")]
+        public AssociationSuffix AssociationSuffix
+        {
+            get { return _associationSuffix; }
+            set { _associationSuffix = value; }
+        }
+
+        #endregion
+    }
+}
---
+++ tags/CSLA-1.1/Source/CodeSmith.SchemaHelper/SearchCriteriaProperty.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+
+using CodeSmith.Engine;
+
+namespace CodeSmith.SchemaHelper
+{
+    [Serializable]
+    [PropertySerializer(typeof(XmlPropertySerializer))]
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public class SearchCriteriaProperty
+    {
+        #region Constructor(s)
+
+        public SearchCriteriaProperty()
+        {
+            SearchCriteria = SearchCriteriaEnum.All;
+            Prefix = "GetBy";
+            Delimeter = String.Empty;
+            Suffix = String.Empty;
+        }
+
+        #endregion
+
+        #region Public Overridden Method(s)
+
+        public override string ToString()
+        {
+            return "(Expand to edit...)";
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        [NotifyParentProperty(true)]
+        [Description("Which SearchCriteria to generate.")]
+        public SearchCriteriaEnum SearchCriteria { get; set; }
+
+        [NotifyParentProperty(true)]
+        [Description("Prefix for a search method.")]
+        public string Prefix { get; set; }
+
+        [NotifyParentProperty(true)]
+        [Description("Delimeter between member names for a search method..")]
+        public string Delimeter { get; set; }
+
+        [NotifyParentProperty(true)]
+        [Description("Suffix for a search method.")]
+        public string Suffix { get; set; }
+
+        #endregion
+    }
+}
