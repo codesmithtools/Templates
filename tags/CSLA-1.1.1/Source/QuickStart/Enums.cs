795cdddd7f80c3786b62fdd1c3092ed5449ae022
bniemyjski <bniemyjski@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2009-10-01 17:32:36 +0000

    Added tag for CSLA version 1.1.1

---
+++ tags/CSLA-1.1.1/Source/QuickStart/Enums.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace QuickStart
+{
+    #region Enumeration(s)
+
+    public enum DataAccessMethod : byte 
+    {
+        None = 0,
+        ParameterizedSQL = 1
+    }
+
+    public enum ProjectTypeEnum : byte
+    {
+        None = 0,
+        DynamicDataWebApp = 1,
+        DynamicDataWebSite = 2
+    }
+
+    #endregion
+}
