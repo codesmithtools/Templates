47625481b6699649e22a619173d30eb72c123e49
Blake Niemyjski <bniemyjski@gmail.com>
2009-10-05 19:34:19 +0000

    Updated Custom Properties Samples.
    Added new DropDownList UITypeEditor Sample.
    Added new sample that shows you how to serialize a custom Dictionary of TableSchema and ViewSchema Pairs.

--- Projects/CSharp/CustomPropertiesSample/ModalEditor/ModalEditorPropertyEditorForm.cs
+++ Projects/CSharp/CustomPropertiesSample/ModalEditor/ModalEditorPropertyEditorForm.cs
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 //
-// Copyright (c) 2002-2008 CodeSmith Tools, LLC.  All rights reserved.
+// Copyright (c) 2002-2009 CodeSmith Tools, LLC.  All rights reserved.
 // 
 // The terms of use for this software are contained in the file
 // named sourcelicense.txt, which can be found in the root of this distribution.
@@ -12,10 +12,6 @@
 //------------------------------------------------------------------------------
 
 using System;
-using System.Drawing;
-using System.Collections;
-using System.ComponentModel;
-using System.Windows.Forms;
 using System.Windows.Forms.Design;
 
 namespace CodeSmith.Samples
@@ -31,7 +27,7 @@ namespace CodeSmith.Samples
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		private System.ComponentModel.Container components = null;
+		private System.ComponentModel.Container components;
 
 		public ModalEditorPropertyEditorForm()
 		{
--- Projects/CSharp/CustomPropertiesSample/PurchaseOrder/PurchaseOrder.cs
+++ Projects/CSharp/CustomPropertiesSample/PurchaseOrder/PurchaseOrder.cs
@@ -1,6 +1,6 @@
 //------------------------------------------------------------------------------
 //
-// Copyright (c) 2002-2008 CodeSmith Tools, LLC.  All rights reserved.
+// Copyright (c) 2002-2009 CodeSmith Tools, LLC.  All rights reserved.
 // 
 // The terms of use for this software are contained in the file
 // named sourcelicense.txt, which can be found in the root of this distribution.
@@ -16,10 +16,10 @@ using System.Xml.Serialization;
 using System.ComponentModel;
 using CodeSmith.CustomProperties;
 
-namespace CodeSmith.SampleCustomProperties
+namespace CodeSmith.Samples
 {
 	[TypeConverter(typeof(XmlSerializedTypeConverter))]
-	[Editor(typeof(CodeSmith.CustomProperties.XmlSerializedFilePicker), typeof(System.Drawing.Design.UITypeEditor))]
+	[Editor(typeof(XmlSerializedFilePicker), typeof(System.Drawing.Design.UITypeEditor))]
 	[XmlRootAttribute("PurchaseOrder", Namespace="http://www.codesmithtools.com/po", IsNullable = false)]
 	public class PurchaseOrder
 	{
@@ -32,7 +32,7 @@ namespace CodeSmith.SampleCustomProperties
 		public decimal TotalCost;
 		public override string ToString()
 		{
-			return "{PO: $" + TotalCost.ToString() + "}";
+			return string.Format("{{PO: ${0}}}", TotalCost);
 		}
 	}
 	
