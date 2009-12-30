b348b64ef4092ed9772409246db4f7e0eebd33fd
bniemyjski <bniemyjski@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2009-12-30 07:13:48 +0000

    Created a CSLA Stable tag.

---
+++ tags/CSLA-Stable/Source/QuickStart/ProjectBuilderSettings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using CodeSmith.Engine;
+using CodeSmith.SchemaHelper;
+
+using SchemaExplorer;
+
+namespace QuickStart
+{
+    public class ProjectBuilderSettings
+    {
+        public DatabaseSchema SourceDatabase { get; set; }
+        public String Location { get; set; }
+        public String SolutionName { get; set; }
+        public LanguageEnum Language { get; set; }
+        public ProjectTypeEnum ProjectType { get; set; }
+        public bool IncludeDataServices { get; set; }
+        public String DataProjectName { get; set; }
+        public bool CopyTemplatesToFolder { get; set; }
+        public String InterfaceProjectName { get; set; }
+        public String TestProjectName { get; set; }
+        public bool IncludeTestProject { get; set; }
+        public CodeTemplate CodeTemplate { get; set; }
+        public string WebSkin { get; set; }
+
+        public string LanguageFolder
+        {
+            get { return (this.Language == LanguageEnum.CSharp) ? "CSharp" : "VisualBasic"; }
+        }
+
+        public string LanguageAppendage
+        {
+            get { return (this.Language == LanguageEnum.CSharp) ? "cs" : "vb"; }
+        }
+
+        public string FileName
+        {
+            get { return "WebApplicationProject"; }
+        }
+
+        public string ZipFileFolder
+        {
+            get { return Path.Combine(CodeTemplate.CodeTemplateInfo.DirectoryName, Path.Combine("Common", LanguageFolder)); }
+        }
+    }
+}
---
+++ tags/CSLA-Stable/Source/QuickStart/SolutionItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using CodeSmith.SchemaHelper;
+
+namespace QuickStart
+{
+    public class SolutionItem
+    {
+        public SolutionItem(string name, string path, LanguageEnum language)
+            : this(name, path, language, false, null)
+        {
+
+        }
+
+        public SolutionItem(string name, string path, LanguageEnum language, bool website, SolutionItem[] projectReferences)
+        {
+            Name = name;
+            Path = path;
+            Language = language;
+            Website = website;
+            ProjectReferences = projectReferences;
+        }
+
+        public SolutionItem[] ProjectReferences { get; set; }
+        public bool Website { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+        private Guid _guid = Guid.NewGuid();
+        public Guid Guid
+        {
+            get { return _guid; }
+            set { _guid = value; }
+        }
+        public string GuidString
+        {
+            get { return _guid.ToString().ToUpper(); }
+        }
+
+        public LanguageEnum Language { get; set; }
+        public string LanguageGuidString
+        {
+            get
+            {
+                return (Language == LanguageEnum.CSharp)
+                    ? "FAE04EC0-301F-11D3-BF4B-00C04F79EFBC"
+                    : "F184B08F-C81C-45F6-A57F-5ABD9991F28F";
+            }
+        }
+
+    }
+}
