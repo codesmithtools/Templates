7d47be5bbb116d48910f76ecd0ba933790c49655
Blake Niemyjski <bniemyjski@gmail.com>
2010-01-25 21:11:17 +0000

    Updated PetShop sample applications. Begin adding unit tests.

--- Projects/Framework-Samples/Csla/CSharp/PetShop/Source/PetShop.Tests/PetShop.Tests.ParameterizedSQL/Properties/AssemblyInfo.cs
+++ Projects/Framework-Samples/Csla/CSharp/PetShop/Source/PetShop.Tests/PetShop.Tests.ParameterizedSQL/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PetShop.Test")]
+[assembly: AssemblyTitle("PetShop.Tests.ParameterizedSQL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("PetShop.Test")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyProduct("PetShop.Tests.ParameterizedSQL")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM componenets.  If you need to access a type in this assembly from 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a49f37cb-5787-4452-81f4-c2bc70bb41fb")]
+[assembly: Guid("e66ca362-4019-40cf-8b94-e857d133de33")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Projects/Framework-Samples/Csla/CSharp/PetShop/Source/PetShop.Tests/PetShop.Tests.StoredProcedures/LineItemTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PetShop.Tests.StoredProcedures
+{
+    [TestFixture]
+    public class LineItemTests
+    {
+        // Test composit keys.
+
+        // Test to make sure that the PK was updated after insert..
+
+    }
+}
