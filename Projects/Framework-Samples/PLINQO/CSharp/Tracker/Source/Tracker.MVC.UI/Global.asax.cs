465e1a8111c73df99d59c0c17a6dc013401895ff
Shannon Davidson <shannondavidson@users.noreply.github.com>
2010-01-26 20:24:12 +0000

    corrected reference issue.

--- Projects/Framework-Samples/PLINQO/CSharp/Tracker/Source/Tracker.MVC.UI/Global.asax.cs
+++ Projects/Framework-Samples/PLINQO/CSharp/Tracker/Source/Tracker.MVC.UI/Global.asax.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using HibernatingRhinos.Profiler.Appender.LinqToSql;
 using PLINQO.Mvc.UI.Binder;
 using Tracker.Core.Data;
 
