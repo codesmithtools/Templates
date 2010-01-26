c5c9585e445cf652bb552fe76af0a71305e8ec75
shannon.davidson <shannon.davidson@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2010-01-26 18:59:57 +0000

    Added a dashboard to demonstrate futures

--- trunk/Projects/Framework-Samples/PLINQO/CSharp/Tracker/Source/Tracker.MVC.UI/Controllers/HomeController.cs
+++ trunk/Projects/Framework-Samples/PLINQO/CSharp/Tracker/Source/Tracker.MVC.UI/Controllers/HomeController.cs
@@ -8,7 +8,9 @@ using System.Web.Mvc;
 using System.Xml;
 using System.Xml.Xsl;
 using CodeSmith.Data.Audit;
+using CodeSmith.Data.Linq;
 using Tracker.Core.Data;
+using Tracker.MVC.UI.Models;
 
 
 namespace PLINQO.Mvc.UI.Controllers
@@ -19,8 +21,25 @@ namespace PLINQO.Mvc.UI.Controllers
         public ActionResult Index()
         {
             ViewData["Message"] = "Welcome to PLINQO!";
-            
-            return View();
+
+            var dashboard = new Dashboard();
+
+            using (var db = new TrackerDataContext())
+            {
+                dashboard.CurrentUser = db.User.GetByEmailAddress(User.Identity.Name);
+                dashboard.TasksNotStarted = db.Task.ByAssignedId(dashboard.CurrentUser.Id).ByStatus(Status.NotStarted).Future();
+                dashboard.TasksInProgress = db.Task.ByAssignedId(dashboard.CurrentUser.Id).ByStatus(Status.InProgress).Future();
+                dashboard.TasksCompleted = db.Task.ByAssignedId(dashboard.CurrentUser.Id).ByStatus(Status.Completed).Future();
+                dashboard.TasksWaitingOnSomeone = db.Task.ByAssignedId(dashboard.CurrentUser.Id).ByStatus(Status.WaitingOnSomeoneElse).Future();
+                dashboard.TotalTasks = db.Task.FutureCount();
+                dashboard.TotalTasksCreatedByMe = db.Task.ByCreatedId(dashboard.CurrentUser.Id).FutureCount();
+                dashboard.TotalTasksAssignedToMe = db.Task.ByAssignedId(dashboard.CurrentUser.Id).FutureCount();
+                dashboard.TotalTasksCompleted = db.Task.ByStatus(Status.Completed).FutureCount();
+
+                db.ExecuteFutureQueries();
+            }
+
+            return View(dashboard);
         }
 
         public ActionResult About()
