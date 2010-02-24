381cce2a484f40b419dbd9beaf54e463187adee6
bniemyjski <bniemyjski@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2010-02-24 00:39:20 +0000

    Added CSLA 2.0.0 tag.

---
+++ tags/CSLA-2.0.0/Source/QuickStart/UnitTestProjectCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace QuickStart
+{
+    public class UnitTestProjectCreator : VsProjectCreator
+    {
+        public UnitTestProjectCreator(ProjectBuilderSettings projectBuilder)
+            : base(projectBuilder) { }
+
+        protected override void GetFiles(PathHelper projectPath)
+        {
+            GetVsProject(projectPath, "TestProject");
+        }
+        protected override void ReplaceVariables(PathHelper projectPath, string projectGuid, string projectName)
+        {
+            VariableUpdateDirectory(projectPath, projectGuid, projectName, this.ProjectBuilder.LanguageAppendage, null);
+        }
+    }
+}
