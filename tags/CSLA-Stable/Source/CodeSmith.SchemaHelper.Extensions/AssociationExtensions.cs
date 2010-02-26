7c0a9962d01473e97806c0065c4c5045de001525
bniemyjski <bniemyjski@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2010-02-26 13:52:59 +0000

    Arg, Updated the branch the old fashion way..

---
+++ tags/CSLA-Stable/Source/CodeSmith.SchemaHelper.Extensions/AssociationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodeSmith.SchemaHelper
+{
+    /// <summary>
+    /// Extension Methods for AssociationExtensions
+    /// </summary>
+    public static class AssociationExtensions
+    {
+        public static string BuildPropertyVariableArguments(this Association association)
+        {
+            string parameters = string.Empty;
+
+            foreach (AssociationMember member in association)
+            {
+                parameters += string.Format(", {0}", member.AssociatedColumn.PropertyName);
+            }
+
+            return parameters.TrimStart(new[] { ',', ' ' });
+        }
+
+        public static string BuildPropertyVariables(this Association association)
+        {
+            string parameters = string.Empty;
+
+            foreach (AssociationMember member in association)
+            {
+                parameters += member.AssociatedColumn.PropertyName;
+            }
+
+            return parameters;
+        }
+    }
+}
