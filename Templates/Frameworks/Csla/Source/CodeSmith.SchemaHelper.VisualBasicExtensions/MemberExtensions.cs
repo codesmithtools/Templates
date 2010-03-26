aea917d003116f8361fa85a4b2d1dc992ffbcf07
Blake Niemyjski <bniemyjski@gmail.com>
2010-03-26 04:39:50 +0000

    Issue 324:	CSLA RegisterProperty for dates causes compile exception.

--- Templates/Frameworks/Csla/Source/CodeSmith.SchemaHelper.VisualBasicExtensions/MemberExtensions.cs
+++ Templates/Frameworks/Csla/Source/CodeSmith.SchemaHelper.VisualBasicExtensions/MemberExtensions.cs
@@ -16,5 +16,23 @@ namespace CodeSmith.SchemaHelper
 
             return string.Empty;
         }
+
+        public static bool CanGenerateNullDefault(this Member member)
+        {
+            bool defaultValueAllowed = member.BaseSystemType == "System.Xml.XmlDocument" ||
+                                       member.BaseSystemType == "System.Guid" ||
+                                       member.BaseSystemType == "System.Byte" ||
+                                       member.SystemType == "System.Byte()" ||
+                                       member.BaseSystemType == "System.DateTime" ||
+                                       member.BaseSystemType == "System.DateTimeOffset" ||
+                                       member.BaseSystemType == "System.Boolean";
+
+            if (member.IsNullable && !defaultValueAllowed)
+            {
+                return true;
+            }
+
+            return false;
+        }
     }
 }
