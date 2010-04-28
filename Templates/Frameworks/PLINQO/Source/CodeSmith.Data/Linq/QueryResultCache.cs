f2132d6e8b049916cb710ec2a878e0bdec5343f1
Paul Welter <pwelter@loresoft.com>
2010-04-28 13:49:46 +0000

    - apply SQLCacheDependency support patch

--- Templates/Frameworks/PLINQO/Source/CodeSmith.Data/Linq/QueryResultCache.cs
+++ Templates/Frameworks/PLINQO/Source/CodeSmith.Data/Linq/QueryResultCache.cs
@@ -9,6 +9,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Xml;
 using CodeSmith.Data.Caching;
+using System.Data.Linq;
 
 namespace CodeSmith.Data.Linq
 {
@@ -50,6 +51,38 @@ namespace CodeSmith.Data.Linq
         /// </summary>
         /// <typeparam name="T">The type of the data in the data source.</typeparam>
         /// <param name="query">The query to be materialized.</param>
+        /// <param name="duration">The amount of time, in seconds, that a cache entry is to remain in the output cache.</param>
+        /// <param name="sqlCacheDependencyTableNames">The table names for which to add SQL Cache Dependencies</param>
+        /// <returns>The result of the query.</returns>
+        public static IEnumerable<T> FromCache<T>(this IQueryable<T> query, int duration,
+            params string[] sqlCacheDependencyTableNames)
+        {
+            CacheSettings cacheSettings = new CacheSettings(duration).AddCacheDependency(query.GetDataContext().Connection.Database, sqlCacheDependencyTableNames);
+            return query.FromCache(cacheSettings);
+        }
+
+        /// <summary>
+        /// Returns the result of the query; if possible from the cache, otherwise
+        /// the query is materialized and the result cached before being returned.
+        /// </summary>
+        /// <typeparam name="T">The type of the data in the data source.</typeparam>
+        /// <param name="query">The query to be materialized.</param>
+        /// <param name="duration">The amount of time, in seconds, that a cache entry is to remain in the output cache.</param>
+        /// <param name="sqlCacheDependencyTables">The tables for which to add SQL Cache Dependencies</param>
+        /// <returns>The result of the query.</returns>
+        public static IEnumerable<T> FromCache<T>(this IQueryable<T> query, int duration,
+            params ITable[] sqlCacheDependencyTables)
+        {
+            CacheSettings cacheSettings = new CacheSettings(duration).AddCacheDependency(query, sqlCacheDependencyTables);
+            return query.FromCache(cacheSettings);
+        }
+
+        /// <summary>
+        /// Returns the result of the query; if possible from the cache, otherwise
+        /// the query is materialized and the result cached before being returned.
+        /// </summary>
+        /// <typeparam name="T">The type of the data in the data source.</typeparam>
+        /// <param name="query">The query to be materialized.</param>
         /// <param name="profileName">Name of the cache profile to use.</param>
         /// <returns>The result of the query.</returns>
         public static IEnumerable<T> FromCache<T>(this IQueryable<T> query, string profileName)
@@ -58,6 +91,37 @@ namespace CodeSmith.Data.Linq
             return query.FromCache(cacheSettings);
         }
 
+        /// <summary>
+        /// Returns the result of the query; if possible from the cache, otherwise
+        /// the query is materialized and the result cached before being returned.
+        /// </summary>
+        /// <typeparam name="T">The type of the data in the data source.</typeparam>
+        /// <param name="query">The query to be materialized.</param>
+        /// <param name="profileName">Name of the cache profile to use.</param>
+        /// <param name="sqlCacheDependencyTables">The tables for which to add SQL Cache Dependencies</param>
+        /// <returns>The result of the query.</returns>
+        public static IEnumerable<T> FromCache<T>(this IQueryable<T> query, string profileName, 
+            params ITable[] sqlCacheDependencyTables)
+        {
+            CacheSettings cacheSettings = CacheManager.GetProfile(profileName).AddCacheDependency(query.GetDataContext().Connection.Database, sqlCacheDependencyTables);
+            return query.FromCache(cacheSettings);
+        }
+
+        /// <summary>
+        /// Returns the result of the query; if possible from the cache, otherwise
+        /// the query is materialized and the result cached before being returned.
+        /// </summary>
+        /// <typeparam name="T">The type of the data in the data source.</typeparam>
+        /// <param name="query">The query to be materialized.</param>
+        /// <param name="profileName">Name of the cache profile to use.</param>
+        /// <param name="sqlCacheDependencyTableNames">The table names for which to add SQL Cache Dependencies</param>
+        /// <returns>The result of the query.</returns>
+        public static IEnumerable<T> FromCache<T>(this IQueryable<T> query, string profileName,
+            params string[] sqlCacheDependencyTableNames)
+        {
+            CacheSettings cacheSettings = CacheManager.GetProfile(profileName).AddCacheDependency(query.GetDataContext().Connection.Database, sqlCacheDependencyTableNames);
+            return query.FromCache(cacheSettings);
+        }
         /// <summary>
         /// Returns the result of the query; if possible from the cache, otherwise
         /// the query is materialized and the result cached before being returned.
@@ -131,6 +195,45 @@ namespace CodeSmith.Data.Linq
         /// </summary>
         /// <typeparam name="T">The type of the data in the data source.</typeparam>
         /// <param name="query">The query to be materialized.</param>
+        /// <param name="duration">The amount of time, in seconds, that a cache entry is to remain in the output cache.</param>
+        /// <param name="sqlCacheDependencyTables">The tables for which to add SQL Cache Dependencies</param>
+        /// <returns>The first or default result of the query.</returns>
+        public static T FromCacheFirstOrDefault<T>(this IQueryable<T> query, int duration,
+            params ITable[] sqlCacheDependencyTables)
+        {
+            CacheSettings cacheSettings = new CacheSettings(duration).AddCacheDependency(query.GetDataContext().Connection.Database, sqlCacheDependencyTables);
+            return query
+                .Take(1)
+                .FromCache(cacheSettings)
+                .FirstOrDefault();
+        }
+        /// <summary>
+        /// Returns the result of the query; if possible from the cache, otherwise
+        /// the query is materialized and the result cached before being returned.
+        /// Queries, caches, and returns only the first entity.
+        /// </summary>
+        /// <typeparam name="T">The type of the data in the data source.</typeparam>
+        /// <param name="query">The query to be materialized.</param>
+        /// <param name="duration">The amount of time, in seconds, that a cache entry is to remain in the output cache.</param>
+        /// <param name="sqlCacheDependencyTableNames">The table names for which to add SQL Cache Dependencies</param>
+        /// <returns>The first or default result of the query.</returns>
+        public static T FromCacheFirstOrDefault<T>(this IQueryable<T> query, int duration,
+            params string[] sqlCacheDependencyTableNames)
+        {
+            CacheSettings cacheSettings = new CacheSettings(duration).AddCacheDependency(query.GetDataContext().Connection.Database, sqlCacheDependencyTableNames);
+            return query
+                .Take(1)
+                .FromCache(cacheSettings)
+                .FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the result of the query; if possible from the cache, otherwise
+        /// the query is materialized and the result cached before being returned.
+        /// Queries, caches, and returns only the first entity.
+        /// </summary>
+        /// <typeparam name="T">The type of the data in the data source.</typeparam>
+        /// <param name="query">The query to be materialized.</param>
         /// <param name="profileName">Name of the cache profile to use.</param>
         /// <returns>The first or default result of the query.</returns>
         public static T FromCacheFirstOrDefault<T>(this IQueryable<T> query, string profileName)
@@ -138,6 +241,45 @@ namespace CodeSmith.Data.Linq
             return query
                 .Take(1)
                 .FromCache(profileName)
+                .FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the result of the query; if possible from the cache, otherwise
+        /// the query is materialized and the result cached before being returned.
+        /// Queries, caches, and returns only the first entity.
+        /// </summary>
+        /// <typeparam name="T">The type of the data in the data source.</typeparam>
+        /// <param name="query">The query to be materialized.</param>
+        /// <param name="profileName">Name of the cache profile to use.</param>
+        /// <param name="sqlCacheDependencyTables">The tables for which to add SQL Cache Dependencies</param>
+        /// <returns>The first or default result of the query.</returns>
+        public static T FromCacheFirstOrDefault<T>(this IQueryable<T> query, string profileName,
+            params ITable[] sqlCacheDependencyTables)
+        {
+            CacheSettings cacheSettings = CacheManager.GetProfile(profileName).AddCacheDependency(query.GetDataContext().Connection.Database, sqlCacheDependencyTables);
+            return query
+                .Take(1)
+                .FromCache(cacheSettings)
+                .FirstOrDefault();
+        }
+        /// <summary>
+        /// Returns the result of the query; if possible from the cache, otherwise
+        /// the query is materialized and the result cached before being returned.
+        /// Queries, caches, and returns only the first entity.
+        /// </summary>
+        /// <typeparam name="T">The type of the data in the data source.</typeparam>
+        /// <param name="query">The query to be materialized.</param>
+        /// <param name="profileName">Name of the cache profile to use.</param>
+        /// <param name="sqlCacheDependencyTableNames">The table names for which to add SQL Cache Dependencies</param>
+        /// <returns>The first or default result of the query.</returns>
+        public static T FromCacheFirstOrDefault<T>(this IQueryable<T> query, string profileName,
+            params string[] sqlCacheDependencyTableNames)
+        {
+            CacheSettings cacheSettings = CacheManager.GetProfile(profileName).AddCacheDependency(query.GetDataContext().Connection.Database, sqlCacheDependencyTableNames);
+            return query
+                .Take(1)
+                .FromCache(cacheSettings)
                 .FirstOrDefault();
         }
 
