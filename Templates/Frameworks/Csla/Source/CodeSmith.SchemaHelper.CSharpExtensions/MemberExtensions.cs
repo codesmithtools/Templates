43e8411d7fd574e925f021acdb319c6c6b856bed
Blake Niemyjski <bniemyjski@gmail.com>
2010-05-20 04:02:55 +0000

    Issue 339: CSLA: support for passing down the parent id to children in a list. (Add new core). Issue 365: CSLA: Nested SwitchableObjects causing issues with Delete. Fixed a critical bug where the FK values were not being updated properly in child collections after Inserts or Updates.

--- Templates/Frameworks/Csla/Source/CodeSmith.SchemaHelper.CSharpExtensions/MemberExtensions.cs
+++ Templates/Frameworks/Csla/Source/CodeSmith.SchemaHelper.CSharpExtensions/MemberExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CodeSmith.SchemaHelper
 {
@@ -12,6 +13,69 @@ namespace CodeSmith.SchemaHelper
             if (member.IsPrimaryKey)
             {
                 return string.Format("\n\t\t[System.ComponentModel.DataObjectField(true, {0})]", member.IsIdentity.ToString().ToLower());
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// This is used in the Insert and Update methods to figure out the association for the a fk member.
+        /// </summary>
+        /// <param name="member"></param>
+        /// <returns></returns>
+        public static string ResolveAssociationPropertyVariable(this Member member)
+        {
+            foreach (Association association in member.Entity.AssociatedManyToOne.Distinct())
+            {
+                foreach (AssociationMember associationMember in association)
+                {
+                    if (member.ColumnName == associationMember.AssociatedColumn.ColumnName && member.TableName == associationMember.AssociatedColumn.TableName)
+                    {
+                        return Util.NamingConventions.VariableName(associationMember.ClassName);
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// This is used in the AddNewCore methods to figure out the association for the a fk member.
+        /// </summary>
+        /// <param name="member"></param>
+        /// <returns></returns>
+        public static string ResolveAssociationPropertyClassName(this Member member)
+        {
+            foreach (Association association in member.Entity.AssociatedManyToOne.Distinct())
+            {
+                foreach (AssociationMember associationMember in association)
+                {
+                    if (member.ColumnName == associationMember.AssociatedColumn.ColumnName && member.TableName == associationMember.AssociatedColumn.TableName)
+                    {
+                        return Util.NamingConventions.PropertyName(associationMember.ClassName);
+                    }
+                }
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// This is used in the Insert and Update methods to figure out the association for the a fk member.
+        /// </summary>
+        /// <param name="member"></param>
+        /// <returns></returns>
+        public static string ResolveAssociationPropertyVariableWithChildProperty(this Member member)
+        {
+            foreach (Association association in member.Entity.AssociatedManyToOne.Distinct())
+            {
+                foreach (AssociationMember associationMember in association)
+                {
+                    if (member.ColumnName == associationMember.AssociatedColumn.ColumnName && member.TableName == associationMember.AssociatedColumn.TableName)
+                    {
+                        return string.Format("{0}.{1}", Util.NamingConventions.VariableName(associationMember.ClassName), Util.NamingConventions.PropertyName(associationMember.ColumnName));
+                    }
+                }
             }
 
             return string.Empty;
