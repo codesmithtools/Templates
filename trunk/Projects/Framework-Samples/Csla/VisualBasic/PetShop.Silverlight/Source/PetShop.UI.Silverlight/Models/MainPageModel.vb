a0d786f4f7c749b74e54f33315f6d86562896e77
bniemyjski <bniemyjski@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2010-07-24 06:17:14 +0000

    Added VB PetShop Silverlight Sample.

---
+++ trunk/Projects/Framework-Samples/Csla/VisualBasic/PetShop.Silverlight/Source/PetShop.UI.Silverlight/Models/MainPageModel.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace PetShop.UI.Silverlight
+    Public Class MainPageModel
+        Inherits FrameworkElement
+        Public Shared ReadOnly CurrentControlProperty As DependencyProperty = DependencyProperty.Register("CurrentControl", GetType(UserControl), GetType(MainPageModel), New PropertyMetadata(Nothing))
+
+        Public Property CurrentControl() As UserControl
+            Get
+                Return DirectCast(GetValue(CurrentControlProperty), UserControl)
+            End Get
+            Set(ByVal value As UserControl)
+                SetValue(CurrentControlProperty, value)
+            End Set
+        End Property
+
+        Private Shared _main As MainPageModel
+        Public Sub New()
+            _main = Me
+            CurrentControl = New CategoryListPage()
+        End Sub
+
+        Public Shared Sub ShowForm(ByVal form As UserControl)
+            _main.CurrentControl = form
+        End Sub
+    End Class
+End Namespace
