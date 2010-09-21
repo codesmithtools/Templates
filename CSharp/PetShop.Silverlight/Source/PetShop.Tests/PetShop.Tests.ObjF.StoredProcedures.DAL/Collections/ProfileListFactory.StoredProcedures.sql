e0688abf82030d151adcccf7e69ae765840b5767
Blake Niemyjski <bniemyjski@gmail.com>
2010-09-21 23:13:23 +0000

    created tag for csla 3.0 samples.

---
+++ CSharp/PetShop.Silverlight/Source/PetShop.Tests/PetShop.Tests.ObjF.StoredProcedures.DAL/Collections/ProfileListFactory.StoredProcedures.sql
@@ -0,0 +1,489 @@
+﻿--region Drop Existing Procedures
+
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+PRINT N'Dropping CSLA_Profile_Insert'
+GO
+IF EXISTS(SELECT 1 FROM fn_listextendedproperty (NULL, 'SCHEMA', 'dbo', 'PROCEDURE', 'CSLA_Profile_Insert', default, default) WHERE name = 'CustomProcedure' and value = '1')
+BEGIN
+    RAISERROR ('The procedure CSLA_Profile_Insert has an Extended Property "CustomProcedure" which means is has been customized. Please review and remove the property if you wish to drop the procedure.',16,1)
+    INSERT INTO #tmpErrors (Error) SELECT 1
+END
+GO
+
+IF OBJECT_ID(N'CSLA_Profile_Insert') IS NOT NULL
+	DROP PROCEDURE CSLA_Profile_Insert
+
+GO
+IF @@ERROR!=0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'The stored procedure drop has succeeded'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'The stored procedure drop has failed'
+GO
+
+DROP TABLE #tmpErrors
+GO
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+PRINT N'Dropping CSLA_Profile_Update'
+GO
+IF EXISTS(SELECT 1 FROM fn_listextendedproperty (NULL, 'SCHEMA', 'dbo', 'PROCEDURE', 'CSLA_Profile_Update', default, default) WHERE name = 'CustomProcedure' and value = '1')
+BEGIN
+    RAISERROR ('The procedure CSLA_Profile_Update has an Extended Property "CustomProcedure" which means is has been customized. Please review and remove the property if you wish to drop the procedure.',16,1)
+    INSERT INTO #tmpErrors (Error) SELECT 1
+END
+GO
+
+IF OBJECT_ID(N'CSLA_Profile_Update') IS NOT NULL
+	DROP PROCEDURE CSLA_Profile_Update
+
+GO
+IF @@ERROR!=0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'The stored procedure drop has succeeded'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'The stored procedure drop has failed'
+GO
+
+DROP TABLE #tmpErrors
+GO
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+PRINT N'Dropping CSLA_Profile_Delete'
+GO
+IF EXISTS(SELECT 1 FROM fn_listextendedproperty (NULL, 'SCHEMA', 'dbo', 'PROCEDURE', 'CSLA_Profile_Delete', default, default) WHERE name = 'CustomProcedure' and value = '1')
+BEGIN
+    RAISERROR ('The procedure CSLA_Profile_Delete has an Extended Property "CustomProcedure" which means is has been customized. Please review and remove the property if you wish to drop the procedure.',16,1)
+    INSERT INTO #tmpErrors (Error) SELECT 1
+END
+GO
+
+IF OBJECT_ID(N'CSLA_Profile_Delete') IS NOT NULL
+	DROP PROCEDURE CSLA_Profile_Delete
+
+GO
+IF @@ERROR!=0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'The stored procedure drop has succeeded'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'The stored procedure drop has failed'
+GO
+
+DROP TABLE #tmpErrors
+GO
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+PRINT N'Dropping CSLA_Profile_Select'
+GO
+IF EXISTS(SELECT 1 FROM fn_listextendedproperty (NULL, 'SCHEMA', 'dbo', 'PROCEDURE', 'CSLA_Profile_Select', default, default) WHERE name = 'CustomProcedure' and value = '1')
+BEGIN
+    RAISERROR ('The procedure CSLA_Profile_Select has an Extended Property "CustomProcedure" which means is has been customized. Please review and remove the property if you wish to drop the procedure.',16,1)
+    INSERT INTO #tmpErrors (Error) SELECT 1
+END
+GO
+
+IF OBJECT_ID(N'CSLA_Profile_Select') IS NOT NULL
+	DROP PROCEDURE CSLA_Profile_Select
+
+GO
+IF @@ERROR!=0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'The stored procedure drop has succeeded'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'The stored procedure drop has failed'
+GO
+
+DROP TABLE #tmpErrors
+GO
+--endregion
+
+GO
+
+
+--region [dbo].[CSLA_Profile_Insert]
+
+------------------------------------------------------------------------------------------------------------------------
+-- Generated By:   Blake Niemyjski using CodeSmith: v5.2.2, CSLA Templates: v3.0.0.0, CSLA Framework: v4.0.0
+-- Procedure Name: [dbo].[CSLA_Profile_Insert]
+------------------------------------------------------------------------------------------------------------------------
+
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+
+PRINT N'Creating [dbo].[CSLA_Profile_Insert]'
+GO
+
+IF EXISTS(SELECT 1 FROM fn_listextendedproperty (NULL, 'SCHEMA', 'dbo', 'PROCEDURE', 'CSLA_Profile_Insert', default, default) WHERE name = 'CustomProcedure' and value = '1')
+    BEGIN
+        RAISERROR ('The procedure [dbo].[CSLA_Profile_Insert] has an Extended Property "CustomProcedure" which means is has been customized. Please review and remove the property if you wish to create the stored procedure.',16,1)
+        INSERT INTO #tmpErrors (Error) SELECT 1
+    END
+GO
+
+CREATE PROCEDURE [dbo].[CSLA_Profile_Insert]
+	@p_Username varchar(256),
+	@p_ApplicationName varchar(256),
+	@p_IsAnonymous bit,
+	@p_LastActivityDate datetime,
+	@p_LastUpdatedDate datetime,
+	@p_UniqueID int OUTPUT
+AS
+
+INSERT INTO [dbo].[Profiles] (
+	[Username],
+	[ApplicationName],
+	[IsAnonymous],
+	[LastActivityDate],
+	[LastUpdatedDate]    
+) VALUES (
+	@p_Username,
+	@p_ApplicationName,
+	@p_IsAnonymous,
+	@p_LastActivityDate,
+	@p_LastUpdatedDate)
+
+SET @p_UniqueID = SCOPE_IDENTITY()
+
+
+
+GO
+IF @@ERROR!=0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'Stored procedure creation succeeded.'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'Stored procedure creation failed.'
+GO
+DROP TABLE #tmpErrors
+GO
+
+--endregion
+
+GO
+
+
+--region [dbo].[CSLA_Profile_Update]
+
+------------------------------------------------------------------------------------------------------------------------
+-- Generated By:   Blake Niemyjski using CodeSmith: v5.2.2, CSLA Templates: v3.0.0.0, CSLA Framework: v4.0.0
+-- Procedure Name: [dbo].[CSLA_Profile_Update]
+------------------------------------------------------------------------------------------------------------------------
+
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+
+PRINT N'Creating [dbo].[CSLA_Profile_Update]'
+GO
+
+IF EXISTS(SELECT 1 FROM fn_listextendedproperty (NULL, 'SCHEMA', 'dbo', 'PROCEDURE', 'CSLA_Profile_Update', default, default) WHERE name = 'CustomProcedure' and value = '1')
+    BEGIN
+        RAISERROR ('The procedure [dbo].[CSLA_Profile_Update] has an Extended Property "CustomProcedure" which means is has been customized. Please review and remove the property if you wish to create the stored procedure.',16,1)
+        INSERT INTO #tmpErrors (Error) SELECT 1
+    END
+GO
+
+CREATE PROCEDURE [dbo].[CSLA_Profile_Update]
+	@p_UniqueID int,
+	@p_Username varchar(256),
+	@p_ApplicationName varchar(256),
+	@p_IsAnonymous bit,
+	@p_LastActivityDate datetime,
+	@p_LastUpdatedDate datetime
+AS
+
+UPDATE [dbo].[Profiles] SET
+	[Username] = @p_Username,
+	[ApplicationName] = @p_ApplicationName,
+	[IsAnonymous] = @p_IsAnonymous,
+	[LastActivityDate] = @p_LastActivityDate,
+	[LastUpdatedDate] = @p_LastUpdatedDate
+WHERE
+	[UniqueID] = @p_UniqueID
+
+
+GO
+IF @@ERROR!=0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'Stored procedure creation succeeded.'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'Stored procedure creation failed.'
+GO
+DROP TABLE #tmpErrors
+GO
+
+--endregion
+
+GO
+
+--region [dbo].[CSLA_Profile_Delete]
+
+------------------------------------------------------------------------------------------------------------------------
+-- Generated By:   Blake Niemyjski using CodeSmith: v5.2.2, CSLA Templates: v3.0.0.0, CSLA Framework: v4.0.0
+-- Procedure Name: [dbo].[CSLA_Profile_Delete]
+------------------------------------------------------------------------------------------------------------------------
+
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+
+PRINT N'Creating [dbo].[CSLA_Profile_Delete]'
+GO
+
+IF EXISTS(SELECT 1 FROM fn_listextendedproperty (NULL, 'SCHEMA', 'dbo', 'PROCEDURE', 'CSLA_Profile_Delete', default, default) WHERE name = 'CustomProcedure' and value = '1')
+    BEGIN
+        RAISERROR ('The procedure [dbo].[CSLA_Profile_Delete] has an Extended Property "CustomProcedure" which means is has been customized. Please review and remove the property if you wish to create the stored procedure.',16,1)
+        INSERT INTO #tmpErrors (Error) SELECT 1
+    END
+GO
+
+CREATE PROCEDURE [dbo].[CSLA_Profile_Delete]
+	@p_UniqueID int
+AS
+
+DELETE FROM
+    [dbo].[Profiles]
+WHERE
+	[UniqueID] = @p_UniqueID
+
+GO
+IF @@ERROR!=0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'Stored procedure creation succeeded.'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'Stored procedure creation failed.'
+GO
+DROP TABLE #tmpErrors
+GO
+
+--endregion
+
+GO
+
+--region [dbo].[CSLA_Profile_Select]
+
+------------------------------------------------------------------------------------------------------------------------
+-- Generated By:   Blake Niemyjski using CodeSmith: v5.2.2, CSLA Templates: v3.0.0.0, CSLA Framework: v4.0.0
+-- Procedure Name: [dbo].[CSLA_Profile_Select]
+------------------------------------------------------------------------------------------------------------------------
+
+SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+IF EXISTS (SELECT * FROM tempdb..sysobjects WHERE id=OBJECT_ID('tempdb..#tmpErrors')) DROP TABLE #tmpErrors
+GO
+CREATE TABLE #tmpErrors (Error int)
+GO
+SET XACT_ABORT ON
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+BEGIN TRANSACTION
+GO
+
+PRINT N'Creating [dbo].[CSLA_Profile_Select]'
+GO
+
+IF EXISTS(SELECT 1 FROM fn_listextendedproperty (NULL, 'SCHEMA', 'dbo', 'PROCEDURE', 'CSLA_Profile_Select', default, default) WHERE name = 'CustomProcedure' and value = '1')
+    BEGIN
+        RAISERROR ('The procedure [dbo].[CSLA_Profile_Select] has an Extended Property "CustomProcedure" which means is has been customized. Please review and remove the property if you wish to create the stored procedure.',16,1)
+        INSERT INTO #tmpErrors (Error) SELECT 1
+    END
+GO
+
+CREATE PROCEDURE [dbo].[CSLA_Profile_Select]
+	@p_UniqueID int = NULL,
+	@p_Username varchar(256) = NULL,
+	@p_ApplicationName varchar(256) = NULL,
+	@p_IsAnonymous bit = NULL,
+	@p_IsAnonymousHasValue BIT = 0,
+	@p_LastActivityDate datetime = NULL,
+	@p_LastActivityDateHasValue BIT = 0,
+	@p_LastUpdatedDate datetime = NULL,
+	@p_LastUpdatedDateHasValue BIT = 0
+AS
+
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+SELECT
+	[UniqueID],
+	[Username],
+	[ApplicationName],
+	[IsAnonymous],
+	[LastActivityDate],
+	[LastUpdatedDate]
+FROM
+    [dbo].[Profiles]
+WHERE
+	([UniqueID] = @p_UniqueID OR @p_UniqueID IS NULL)
+	AND ([Username] = @p_Username OR @p_Username IS NULL)
+	AND ([ApplicationName] = @p_ApplicationName OR @p_ApplicationName IS NULL)
+	AND ([IsAnonymous] = @p_IsAnonymous OR (@p_IsAnonymous IS NULL AND @p_IsAnonymousHasValue = 0))
+	AND ([LastActivityDate] = @p_LastActivityDate OR (@p_LastActivityDate IS NULL AND @p_LastActivityDateHasValue = 0))
+	AND ([LastUpdatedDate] = @p_LastUpdatedDate OR (@p_LastUpdatedDate IS NULL AND @p_LastUpdatedDateHasValue = 0))
+
+GO
+IF @@ERROR!=0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
+
+IF EXISTS (SELECT * FROM #tmpErrors) ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT>0 BEGIN
+PRINT 'Stored procedure creation succeeded.'
+COMMIT TRANSACTION
+END
+ELSE PRINT 'Stored procedure creation failed.'
+GO
+DROP TABLE #tmpErrors
+GO
+
+--endregion
+
+GO
+
