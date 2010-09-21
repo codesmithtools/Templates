f720e776064f93ccd3c79c5d4017d50d69b2c7fe
bniemyjski <bniemyjski@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2010-09-21 23:11:42 +0000

    Created tag for CSLA 3.0

---
+++ tags/CSLA-3.0.0/Source/QuickStart/PathHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace QuickStart
+{
+    public static class PathHelper
+    {
+        public static IEnumerable<string> GetFiles(string directory, SearchOption searchOption, params string[] searchPatterns)
+        {
+            var files = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            foreach (var pattern in searchPatterns)
+                files.UnionWith(Directory.GetFiles(directory, pattern, SearchOption.AllDirectories));
+
+            return files;
+        }
+    }
+
+}
