f6c94e7a8cd70718d4d864028793a256982ed6eb
bniemyjski <bniemyjski@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2010-09-21 23:11:08 +0000

    Created tag for CSLA 3.0.1

---
+++ tags/CSLA-3.0.1/Source/CodeSmith.QuickStart/EntitiesCodeTemplate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CodeSmith.QuickStart
+{
+    public class EntitiesCodeTemplate : CSLAMasterTemplate
+    {
+        #region Constructor(s)
+
+        public EntitiesCodeTemplate()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ tags/CSLA-3.0.1/Source/CodeSmith.SchemaHelper/Extensions/AssociationCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CodeSmith.SchemaHelper
+{
+    /// <summary>
+    /// Extension Methods for AssociationExtensions
+    /// </summary>
+    public static class AssociationCollectionExtensions
+    {
+        public static List<Association> Distinct(this List<Association> associations)
+        {
+            return associations.Distinct<Association>(new DistinctAssociationComparer()).ToList();
+        }
+
+        public class DistinctAssociationComparer : IEqualityComparer<Association>
+        {
+            public bool Equals(Association x, Association y)
+            {
+                return x.ClassName == y.ClassName;
+            }
+
+            public int GetHashCode(Association obj)
+            {
+                return obj.ClassName.GetHashCode();
+            }
+        }
+    }
+}
