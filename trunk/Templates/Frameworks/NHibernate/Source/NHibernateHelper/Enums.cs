610648fd772a059cee53bab601da4fcc8583b1c0
tdupont750@gmail.com <tdupont750@gmail.com@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2010-11-16 17:49:52 +0000

    Updated NHibernate 2.1 to 2.1.2. Removed NHibernate 2.0 references. Added Visual Studio 2010 Enum

--- trunk/Templates/Frameworks/NHibernate/Source/NHibernateHelper/Enums.cs
+++ trunk/Templates/Frameworks/NHibernate/Source/NHibernateHelper/Enums.cs
@@ -8,14 +8,14 @@ namespace NHibernateHelper
     public enum NHibernateVersion
     {
         v1_2,
-        v2_0,
         v2_1
     }
 
     public enum VisualStudioVersion
     {
         VS_2005,
-        VS_2008
+        VS_2008,
+        VS_2010
     }
 
     public enum MethodNameGenerationMode
