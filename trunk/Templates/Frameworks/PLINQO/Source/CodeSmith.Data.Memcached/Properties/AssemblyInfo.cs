8505a766d8a605bcbbcccf3dae2617f9a5f0dbf5
bniemyjski <bniemyjski@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2011-01-07 15:59:31 +0000

    Add MemCached Provider to PLINQO

---
+++ trunk/Templates/Frameworks/PLINQO/Source/CodeSmith.Data.Memcached/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CodeSmith.Data.Memcached")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("fa4c1561-f2d5-4f31-9f3f-0f85f7b35dfa")]
