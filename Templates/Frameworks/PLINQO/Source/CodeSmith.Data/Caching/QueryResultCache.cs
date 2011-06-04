d1a878f235e27996885b6aee2cb11e5fbc6db05b
Tom DuPont <tdupont750@gmail.com>
2011-06-04 02:21:15 +0000

    Began updating CodeSmith.Data to support multiple frameworks.

---
+++ Templates/Frameworks/PLINQO/Source/CodeSmith.Data/Caching/QueryResultCache.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+using System.Text;
+using CodeSmith.Data.Linq;
+
+namespace CodeSmith.Data.Caching
+{
+    public class LinqToSqlQueryResultCache : QueryResultCache
+    {
+        public static QueryResultCache Instance { get; private set; }
+
+        static LinqToSqlQueryResultCache()
+        {
+            Instance = new LinqToSqlQueryResultCache();
+        }
+
+        private LinqToSqlQueryResultCache()
+        {
+        }
+
+        protected override string GetDbName(IQueryable query)
+        {
+            var db = query.GetDataContext();
+
+            return db != null && db.Connection != null
+                ? db.Connection.ConnectionString
+                : String.Empty;
+        }
+
+        protected override void Detach<T>(ICollection<T> results, IQueryable query)
+        {
+            foreach (var item in results)
+            {
+                var entity = item as ILinqEntity;
+                if (entity != null)
+                    entity.Detach();
+            }
+        }
+    }
+
+    public abstract class QueryResultCache
+    {
+        /// <summary>
+        /// Returns the result of the query; if possible from the cache, otherwise
+        /// the query is materialized and the result cached before being returned.
+        /// </summary>
+        /// <typeparam name="T">The type of the data in the data source.</typeparam>
+        /// <param name="query">The query to be materialized.</param>
+        /// <param name="settings">Cache settings object.</param>
+        /// <returns>The result of the query.</returns>
+        public IEnumerable<T> FromCache<T>(IQueryable<T> query, CacheSettings settings)
+        {
+            if (settings == null)
+                settings = CacheManager.GetProfile();
+
+            var key = GetHashKey(query);
+
+            // try to get the query result from the cache
+            var result = GetResultCache<T>(key, settings);
+
+            if (result != null)
+                return result;
+
+            // materialize the query
+            result = query.ToList();
+
+            SetResultCache(key, settings, result);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Gets a unique Md5 key for a query.
+        /// </summary>
+        /// <param name="query">The query to build a key from.</param>
+        /// <returns>A Md5 hash unique to the query.</returns>
+        public string GetHashKey(IQueryable query)
+        {
+            // locally evaluate as much of the query as possible
+            var expression = Evaluator.PartialEval(
+                query.Expression,
+                CanBeEvaluatedLocally);
+
+            // use the string representation of the query for the cache key
+            var key = expression.ToString();
+
+            // make key DB specific
+            var dbName = GetDbName(query);
+            if (!String.IsNullOrEmpty(dbName))
+                key += dbName;
+
+            // the key is potentially very long, so use an md5 fingerprint
+            // (fine if the query result data isn't critically sensitive)
+            return ToMd5Fingerprint(key);
+        }
+
+        public void SetResultCache<T>(string key, CacheSettings settings, ICollection<T> results)
+        {
+            SetResultCache(key, settings, results, null);
+        }
+
+        public void SetResultCache<T>(string key, CacheSettings settings, ICollection<T> results, IQueryable query)
+        {
+            if (settings == null)
+                settings = CacheManager.GetProfile();
+
+            if (results == null)
+                return;
+
+            // Don't cache empty result.
+            if (results.Count == 0 && !settings.CacheEmptyResult)
+                return;
+
+            // detach for cache
+            Detach(results, query);
+
+            ICacheProvider cacheProvider = CacheManager.GetProvider(settings.Provider);
+            cacheProvider.Set(key, results, settings);
+        }
+
+        public ICollection<T> GetResultCache<T>(string key, CacheSettings settings)
+        {
+            if (settings == null)
+                settings = CacheManager.GetProfile();
+
+            ICacheProvider cacheProvider = CacheManager.GetProvider(settings.Provider);
+            var collection = cacheProvider.Get<ICollection<T>>(key, settings.Group);
+            return collection;
+        }
+
+        protected abstract void Detach<T>(ICollection<T> results, IQueryable query);
+
+        protected abstract string GetDbName(IQueryable query);
+
+        private static Func<Expression, bool> CanBeEvaluatedLocally
+        {
+            get
+            {
+                return expression =>
+                {
+                    // don't evaluate parameters
+                    if (expression.NodeType == ExpressionType.Parameter)
+                        return false;
+
+                    // can't evaluate queries
+                    if (typeof(IQueryable).IsAssignableFrom(expression.Type))
+                        return false;
+
+                    return true;
+                };
+            }
+        }
+
+        private static string ToMd5Fingerprint(string s)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(s.ToCharArray());
+            byte[] hash = new MD5CryptoServiceProvider().ComputeHash(bytes);
+
+            // concat the hash bytes into one long string
+            return hash.Aggregate(new StringBuilder(32),
+                (sb, b) => sb.Append(b.ToString("X2")))
+                .ToString();
+        }
+    }
+}
