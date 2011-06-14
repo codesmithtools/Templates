2216faba93bdc3377857d7583b053561d5bd1112
Tom DuPont <tdupont750@gmail.com>
2011-06-14 20:24:45 +0000

    Updated CodeSmith.Data.NHibernate to support new Future Architecture.

---
+++ Templates/Frameworks/PLINQO-NH/Source/CodeSmith.Data.NHibernate/Entities/Tables.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate.Linq;
+
+namespace CodeSmith.Data.NHibernate
+{
+    public class Table<T> : ITable<T>
+        where T : class
+    {
+        public Table(INHibernateDataContext dataContext)
+        {
+            DataContext = dataContext;
+        }
+
+        public INHibernateDataContext DataContext { get; private set; }
+
+        #region IQueryable Methods
+
+        private IQueryable<T> GetQueryable()
+        {
+            return DataContext.ObjectTrackingEnabled
+                ? DataContext.Advanced.Session.Query<T>()
+                : DataContext.Advanced.StatelessSession.Query<T>();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return GetQueryable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetQueryable().GetEnumerator();
+        }
+
+        public Type ElementType
+        {
+            get { return GetQueryable().ElementType; }
+        }
+
+        public System.Linq.Expressions.Expression Expression
+        {
+            get { return GetQueryable().Expression; }
+        }
+
+        public IQueryProvider Provider
+        {
+            get { return GetQueryable().Provider; }
+        }
+
+        #endregion
+
+        #region Table Methods
+
+        public void InsertOnSubmit(T entity)
+        {
+            DataContext.Advanced.DefaultSession.Save(entity);
+        }
+
+        public void InsertAllOnSubmit(IEnumerable<T> entities)
+        {
+            foreach (var entity in entities)
+                InsertOnSubmit(entity);
+        }
+
+        public void DeleteOnSubmit(T entity)
+        {
+            DataContext.Advanced.DefaultSession.Delete(entity);
+        }
+
+        public void DeleteAllOnSubmit(IEnumerable<T> entities)
+        {
+            foreach (var entity in entities)
+                DeleteOnSubmit(entity);
+        }
+
+        public void Attach(T entity)
+        {
+            if (!DataContext.ObjectTrackingEnabled)
+                throw new Exception("Can not attach to DataContext when ObjectTrackingEnabled is false.");
+
+            DataContext.Advanced.Session.Update(entity);
+        }
+
+        public void AttachAll(IEnumerable<T> entities)
+        {
+            foreach (var entity in entities)
+                Attach(entity);
+        }
+
+        public void Detach(T entity)
+        {
+            if (DataContext.ObjectTrackingEnabled)
+                DataContext.Advanced.Session.Evict(entity);
+        }
+
+        public void DetachAll(IEnumerable<T> entities)
+        {
+            foreach (var entity in entities)
+                Detach(entity);
+        }
+
+        #endregion
+    }
+}
---
+++ Templates/Frameworks/PLINQO-NH/Source/CodeSmith.Data.NHibernate/Future/NHibernateFutureValue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using CodeSmith.Data.Caching;
+using CodeSmith.Data.Future;
+using CodeSmith.Data.Linq;
+using NHibernate.Linq;
+
+namespace CodeSmith.Data.NHibernate
+{
+    internal class NHibernateFutureValue<T> : IFutureValue<T>, INHibernateFutureQuery
+    {
+        private readonly CacheSettings _cacheSettings;
+
+        private readonly Action _action;
+
+        private readonly global::NHibernate.IFutureValue<T> _value;
+
+        public NHibernateFutureValue(IQueryable<T> query, CacheSettings cacheSettings, Action action)
+        {
+            _action = action;
+            _cacheSettings = cacheSettings;
+            _value = query.ToFutureValue();
+            Query = query;
+        }
+
+        public bool IsLoaded { get; set; }
+
+        public IQueryable Query { get; private set; }
+        
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        public T Value
+        {
+            get
+            {
+                _action.Invoke();
+                return _value.Value;
+            }
+        }
+        
+        public static implicit operator T(NHibernateFutureValue<T> futureValue)
+        {
+            return futureValue.Value;
+        }
+
+        public void Load()
+        {
+            var value = _value.Value;
+
+            if (_cacheSettings == null)
+                return;
+
+            var key = Query.GetHashKey();
+            QueryResultCache.SetResultCache(key, _cacheSettings, new[] { value }, Query);
+        }
+    }
+}
