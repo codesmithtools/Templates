f0e0b13b7c5a8794812d27f263ec86c41da80974
tdupont750@gmail.com <tdupont750@gmail.com@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2011-06-14 20:24:45 +0000

    Updated CodeSmith.Data.NHibernate to support new Future Architecture.

---
+++ trunk/Templates/Frameworks/PLINQO-NH/Source/CodeSmith.Data.NHibernate/DataContext/INHibernateDataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NHibernate;
+
+namespace CodeSmith.Data.NHibernate
+{
+    public interface INHibernateDataContext : IDataContext
+    {
+        bool IsOpen { get; }
+
+        INHibernateDataContextAdvanced Advanced { get; }
+    }
+}
---
+++ trunk/Templates/Frameworks/PLINQO-NH/Source/CodeSmith.Data.NHibernate/DataContext/INHibernateDataContextAdvanced.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeSmith.Data.Future;
+using NHibernate;
+
+namespace CodeSmith.Data.NHibernate
+{
+    public interface INHibernateDataContextAdvanced : IDisposable
+    {
+        bool HasSession { get; }
+
+        ISession Session { get; }
+
+        bool HasStatelessSession { get; }
+
+        IStatelessSession StatelessSession { get; }
+
+        bool HasDefaultSession { get; }
+
+        IStateSession DefaultSession { get; }
+
+        IList<INHibernateFutureQuery> FutureQueries { get; }
+
+        void ExecuteFutureQueries();
+    }
+}
