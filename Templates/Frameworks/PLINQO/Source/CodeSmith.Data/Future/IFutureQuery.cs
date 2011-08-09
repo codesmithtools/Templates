32e378a4d6290e11e196746725ac143f759b8e75
Blake Niemyjski <bniemyjski@gmail.com>
2011-08-09 17:38:34 +0000

    Changes for CodeSmith Generator 6.0 ;) *Breaking*

--- Templates/Frameworks/PLINQO/Source/CodeSmith.Data/Future/IFutureQuery.cs
+++ Templates/Frameworks/PLINQO/Source/CodeSmith.Data/Future/IFutureQuery.cs
@@ -3,7 +3,12 @@ using System.Linq;
 
 namespace CodeSmith.Data.Future
 {
+#if v40
+    // http://blogs.msdn.com/b/csharpfaq/archive/2010/02/16/covariance-and-contravariance-faq.aspx
     public interface IFutureQuery<out T> : IFutureQuery, IEnumerable<T>
+#else
+    public interface IFutureQuery<T> : IFutureQuery, IEnumerable<T>
+#endif
     {
     }
 
