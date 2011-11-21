71f8f3e7191b81200165efbd08ee8f698684740c
Eric J. Smith <eric@codesmithtools.com>
2011-11-21 22:55:26 +0000

    Fix bug with cache that was causing it to fail to remove the correct cache entry.

--- Templates/Frameworks/PLINQO/Source/CodeSmith.Data/Caching/QueryResultCache.cs
+++ Templates/Frameworks/PLINQO/Source/CodeSmith.Data/Caching/QueryResultCache.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -42,9 +43,9 @@ namespace CodeSmith.Data.Caching
             var key = expression.ToString();
 
             // make key DB specific
-            var dbName = GetConnectionString(query);
-            if (!String.IsNullOrEmpty(dbName))
-                key += dbName;
+            var info = GetConnectionUniqueInfo(query);
+            if (!String.IsNullOrEmpty(info))
+                key += "||" + info;
 
             // the key is potentially very long, so use an md5 fingerprint
             // (fine if the query result data isn't critically sensitive)
@@ -94,12 +95,56 @@ namespace CodeSmith.Data.Caching
             db.Detach(results);
         }
 
-        private static string GetConnectionString(IQueryable query)
+        private static readonly ConcurrentDictionary<string, string> _uniqueInfos = new ConcurrentDictionary<string, string>();
+        private static string GetConnectionUniqueInfo(IQueryable query)
         {
             var db = DataContextProvider.GetDataConext(query);
-            return db == null
+            string connectionString = db == null
                 ? String.Empty
                 : db.ConnectionString;
+
+            if (String.IsNullOrEmpty(connectionString))
+                return "NA";
+
+            // cache so that we aren't parsing config string every single time.
+            return _uniqueInfos.GetOrAdd(connectionString, s => {
+                var sb = new StringBuilder();
+
+                var config = ParseConfigValues(connectionString);
+                if (config.ContainsKey("database"))
+                    sb.Append(config["database"]);
+
+                if (config.ContainsKey("initial catalog"))
+                    sb.Append(config["initial catalog"]);
+
+                if (config.ContainsKey("data source"))
+                    sb.Append(config["data source"]);
+
+                if (config.ContainsKey("server"))
+                    sb.Append(config["server"]);
+
+                if (config.ContainsKey("hostname"))
+                    sb.Append(config["hostname"]);
+
+                if (sb.Length == 0)
+                    sb.Append("NA");
+
+                return sb.ToString();
+            });
+        }
+
+        private static Dictionary<string, string> ParseConfigValues(string value) {
+            var attributes = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+            string[] keyValuePairs = value.Split(';');
+            foreach (string t in keyValuePairs) {
+                string[] keyValuePair = t.Split('=');
+                if (keyValuePair.Length == 2)
+                    attributes.Add(keyValuePair[0].Trim(), keyValuePair[1].Trim());
+                else
+                    attributes.Add(t.Trim(), null);
+            }
+
+            return attributes;
         }
 
         private static Func<Expression, bool> CanBeEvaluatedLocally
