37760bf3d2c5d069b2b3f1eda7654aeb01f219c1
Blake Niemyjski <bniemyjski@gmail.com>
2011-12-07 17:22:31 +0000

    Added versioning support to the CodeSmith Samples.

--- Projects/CSharp/CustomPropertiesSample/Properties/AssemblyInfo.cs
+++ Projects/CSharp/CustomPropertiesSample/Properties/AssemblyInfo.cs
@@ -1,15 +1,22 @@
-﻿using System.Reflection;
+﻿//------------------------------------------------------------------------------
+//
+// Copyright (c) 2002-2012 CodeSmith Tools, LLC.  All rights reserved.
+// 
+// The terms of use for this software are contained in the file
+// named sourcelicense.txt, which can be found in the root of this distribution.
+// By using this software in any fashion, you are agreeing to be bound by the
+// terms of this license.
+// 
+// You must not remove this notice, or any other, from this software.
+//
+//------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CodeSmith.Samples.dll")]
-[assembly: AssemblyDescription("CodeSmith is a template-based code generation system.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("CodeSmith Tools, LLC")]
-[assembly: AssemblyProduct("CodeSmith")]
-[assembly: AssemblyCopyright("Copyright (c) 2002-2011 CodeSmith Tools, LLC.  All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
+[assembly: AssemblyTitle("CodeSmith Generator Samples")]
+[assembly: AssemblyDescription("CodeSmith Generator custom PropertyGrid samples.")]
+[assembly: ComVisible(true)]
 [assembly: Guid("9cf405ed-5c12-4ce8-bd2c-d5fb14ad1f18")]
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: CLSCompliant(false)]
