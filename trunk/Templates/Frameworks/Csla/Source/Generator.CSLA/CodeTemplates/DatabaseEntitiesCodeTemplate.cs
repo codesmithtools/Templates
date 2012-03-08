fa5679d153e26e1a45b0c5ffb7bed5b70ad1ac5b
bniemyjski@gmail.com <bniemyjski@gmail.com@6bc54abf-244d-0410-85ef-f5aa4abe7f99>
2012-03-08 19:30:02 +0000

    Update issue 628,613,553 Fixed a bug where IsManyToMany wasn't checking to see if at least two FK's existed in some cases. Fixed a bug with RunLocal.

--- trunk/Templates/Frameworks/Csla/Source/Generator.CSLA/CodeTemplates/DatabaseEntitiesCodeTemplate.cs
+++ trunk/Templates/Frameworks/Csla/Source/Generator.CSLA/CodeTemplates/DatabaseEntitiesCodeTemplate.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Text.RegularExpressions;
+using CodeSmith.CustomProperties;
 using CodeSmith.Engine;
 using CodeSmith.SchemaHelper;
 using SchemaExplorer;
+using Configuration = CodeSmith.SchemaHelper.Configuration;
 
 namespace Generator.CSLA.CodeTemplates
 {
     public class DatabaseEntitiesCodeTemplate : EntitiesCodeTemplate
     {
-        public DatabaseEntitiesCodeTemplate(){}
-
         private List<IEntity> _entities = new List<IEntity>();
         private DatabaseSchema _database;
+
+        public DatabaseEntitiesCodeTemplate()
+        {
+            CleanExpressions = new StringCollection();
+            IgnoreExpressions = new StringCollection();
+        }
 
         [Category("1. DataSource")]
         [Description("Source Database")]
@@ -21,7 +28,7 @@ namespace Generator.CSLA.CodeTemplates
             get { return _database; }
             set
             {
-                if (value != null && value != _database)
+                if (value != null && (_database == null || (_database != null && value.Name != _database.Name && value.ConnectionString != _database.ConnectionString)))
                 {
                     _database = value;
                     if (!_database.DeepLoad)
@@ -35,57 +42,37 @@ namespace Generator.CSLA.CodeTemplates
             }
         }
 
-        public virtual void OnDatabaseChanged()
+        [Category("1. DataSource")]
+        [Description("List of regular expressions to clean table, view and column names.")]
+        [Optional]
+        [DefaultValue("^(sp|tbl|udf|vw)_")]
+        public StringCollection CleanExpressions { get; set; }
+
+        [Category("1. DataSource")]
+        [Description("List of regular expressions to ignore tables when generating.")]
+        [Optional]
+        [DefaultValue("sysdiagrams$")]
+        public StringCollection IgnoreExpressions { get; set; }
+
+        [Optional]
+        [Category("1. DataSource")]
+        [Description("Include views that have an extended property declaring it's business type")]
+        public bool IncludeViews
         {
-            using (TemplateContext.SetContext(this))
-            {
-                if (SourceDatabase == null)
-                    return;
-
-                var provider = new SchemaExplorerEntityProvider(SourceDatabase);
-                _entities = new EntityManager(provider).Entities;
-
-                if(State == TemplateState.RestoringProperties)
-                    return;
-
-                if (_dontPopulateDefaultValues)
-                    return;
-
-                if (DynamicRoot.Count == 0 &&
-                    EditableChild.Count == 0 &&
-                    EditableRoot.Count == 0 &&
-                    ReadOnlyChild.Count == 0 &&
-                    ReadOnlyRoot.Count == 0 &&
-                    SwitchableObject.Count == 0 &&
-                    DynamicListBase.Count == 0 &&
-                    DynamicRootList.Count == 0 &&
-                    EditableRootList.Count == 0 &&
-                    EditableChildList.Count == 0 &&
-                    ReadOnlyList.Count == 0 &&
-                    ReadOnlyChildList.Count == 0)
-                {
-                    PopulateDefaultEntities(_entities);
-                }
-                else
-                {
-                    Refresh();
-                }
-            }
+            get { return Configuration.Instance.IncludeViews; }
+            set { Configuration.Instance.IncludeViews = value; }
         }
 
-        private bool _dontPopulateDefaultValues;
-        private void EnsureEntitiesExists(TableSchemaCollection value)
+        [Optional]
+        [Category("1. DataSource")]
+        [Description("Include stored procedures that have an extended property declaring it's business type")]
+        public bool IncludeFunctions
         {
-            if (_entities != null && _entities.Count > 0)
-                return;
-
-            if (value == null || value.Count <= 0)
-                return;
-
-            _dontPopulateDefaultValues = true;
-            SourceDatabase = value[0].Database;
-            _dontPopulateDefaultValues = false;
+            get { return Configuration.Instance.IncludeFunctions; }
+            set { Configuration.Instance.IncludeFunctions = value; }
         }
+
+        #region Business Object Selections
 
         [Category("6a. Entities")]
         [Description("CommandObject")]
@@ -318,6 +305,105 @@ namespace Generator.CSLA.CodeTemplates
                     NameValueListEntities = new EntityManager(new CSLAEntityProvider(_entities, value, Constants.NameValueList)).Entities;
                 }
             }
+        }
+
+        #endregion
+
+        private bool _dontPopulateDefaultValues;
+        private void EnsureEntitiesExists(TableSchemaCollection value)
+        {
+            // If entities exist then return.
+            if (_entities != null && _entities.Count > 0)
+                return;
+
+            // The entities haven't been set yet, so lets set them if there is atleast one table passed in.
+            if (value == null || value.Count <= 0)
+                return;
+
+            _dontPopulateDefaultValues = true;
+            SourceDatabase = value[0].Database;
+            _dontPopulateDefaultValues = false;
+        }
+
+        public virtual void OnDatabaseChanged()
+        {
+            using (TemplateContext.SetContext(this))
+            {
+                if (!UpdateEntities()) 
+                    return;
+
+                if (State == TemplateState.RestoringProperties)
+                    return;
+
+                if (_dontPopulateDefaultValues)
+                    return;
+
+                if (DynamicRoot.Count == 0 &&
+                    EditableChild.Count == 0 &&
+                    EditableRoot.Count == 0 &&
+                    ReadOnlyChild.Count == 0 &&
+                    ReadOnlyRoot.Count == 0 &&
+                    SwitchableObject.Count == 0 &&
+                    DynamicListBase.Count == 0 &&
+                    DynamicRootList.Count == 0 &&
+                    EditableRootList.Count == 0 &&
+                    EditableChildList.Count == 0 &&
+                    ReadOnlyList.Count == 0 &&
+                    ReadOnlyChildList.Count == 0)
+                {
+                    PopulateDefaultEntities(_entities);
+                }
+                else
+                {
+                    Refresh();
+                }
+            }
+        }
+
+        protected override void OnPropertiesLoaded()
+        {
+            if (TemplateContext.Current == null && UpdateEntities())
+                Refresh();
+
+            base.OnPropertiesLoaded();
+        }
+
+        private bool UpdateEntities()
+        {
+            if (SourceDatabase == null)
+                return false;
+
+            if (CleanExpressions.Count == 0)
+                CleanExpressions.Add("^(sp|tbl|udf|vw)_");
+
+            Configuration.Instance.CleanExpressions.Clear();
+            foreach (string clean in CleanExpressions)
+            {
+                if (!string.IsNullOrEmpty(clean))
+                {
+                    Configuration.Instance.CleanExpressions.Add(new Regex(clean, RegexOptions.IgnoreCase));
+                }
+            }
+
+            if (IgnoreExpressions.Count == 0)
+            {
+                IgnoreExpressions.Add("sysdiagrams$");
+                IgnoreExpressions.Add("^dbo.aspnet");
+            }
+
+            Configuration.Instance.IgnoreExpressions.Clear();
+            foreach (string ignore in IgnoreExpressions)
+            {
+                if (!string.IsNullOrEmpty(ignore))
+                {
+                    Configuration.Instance.IgnoreExpressions.Add(new Regex(ignore, RegexOptions.IgnoreCase));
+                }
+            }
+
+            var provider = new SchemaExplorerEntityProvider(SourceDatabase);
+            _entities = new EntityManager(provider).Entities;
+
+            return true;
         }
 
         private void Refresh()
