74d4730ab87d7ab0268a75b990da24abc99301ca
Blake Niemyjski <bniemyjski@gmail.com>
2012-04-24 15:17:56 +0000

    [Issue 642] The NHibernateProvider was not using read-only access to read the contents of the HBM Files.

--- Templates/Frameworks/PLINQO-NH/Source/CodeSmith.SchemaHelper.NHibernate/NHibernateProvider.cs
+++ Templates/Frameworks/PLINQO-NH/Source/CodeSmith.SchemaHelper.NHibernate/NHibernateProvider.cs
@@ -8,6 +8,11 @@ namespace CodeSmith.SchemaHelper.NHibernate
 {
     public class NHibernateProvider : IEntityProvider
     {
+        private bool _intialized;
+        private string[] _entityPaths;
+        private string[] _viewPaths;
+        private string[] _functionPaths;
+
         public const string EntitiesSubFolder = "Entities";
         public const string ViewsSubFolder = "Views";
         public const string FunctionsSubFolder = "Functions";
@@ -23,87 +28,16 @@ namespace CodeSmith.SchemaHelper.NHibernate
         public string ViewsDirectory { get; private set; }
         public string FunctionsDirectory { get; private set; }
 
-        public string Description
-        {
-            get { return "Entity Provider for PLINQO for NHibernate"; }
-        }
-
-        public void Load()
-        {
-            Initialize();
-
-            if (_entityPaths != null)
-                foreach (var file in _entityPaths)
-                    using (var stream = new FileStream(file, FileMode.Open))
-                    using (var reader = XmlReader.Create(stream))
-                        try
-                        {
-                            var doc = XDocument.Load(reader);
-                            var entity = new NHibernateEntity(doc, Path.GetFileName(file));
-                            EntityStore.Instance.EntityCollection.Add(entity.Name, entity);
-                            
-                        }
-                        catch (Exception ex)
-                        {
-                            Trace.WriteLine("Unable to load file: " + file);
-                            Trace.WriteLine(ex.ToString());
-
-                            Debug.WriteLine("Unable to load file " + file);
-                            Debug.WriteLine(ex.ToString());
-                        }
-
-            if (_viewPaths != null)
-                foreach (var file in _viewPaths)
-                    using (var stream = new FileStream(file, FileMode.Open))
-                    using (var reader = XmlReader.Create(stream))
-                        try
-                        {
-                            var doc = XDocument.Load(reader);
-                            var entity = new NHibernateEntity(doc, Path.GetFileName(file), true);
-                            EntityStore.Instance.EntityCollection.Add(entity.Name, entity);
-                        }
-                        catch (Exception ex)
-                        {
-                            Trace.WriteLine("Unable to load file " + file);
-                            Trace.WriteLine(ex.ToString());
-
-                            Debug.WriteLine("Unable to load file " + file);
-                            Debug.WriteLine(ex.ToString());
-                        }
-
-            if (_functionPaths != null)
-                foreach (var file in _functionPaths)
-                    using (var stream = new FileStream(file, FileMode.Open))
-                    using (var reader = XmlReader.Create(stream))
-                        try
-                        {
-                            var doc = XDocument.Load(reader);
-                            var entity = new NHibernateCommandEntity(doc, Path.GetFileName(file));
-                            EntityStore.Instance.EntityCollection.Add(entity.Name, entity);
-                        }
-                        catch (Exception ex)
-                        {
-                            Trace.WriteLine("Unable to load file: " + file);
-                            Trace.WriteLine(ex.ToString());
-
-                            Debug.WriteLine("Unable to load file " + file);
-                            Debug.WriteLine(ex.ToString());
-                        }
-
-            foreach (var entity in EntityStore.Instance.EntityCollection.Values)
-                entity.Initialize();
-
-            foreach (var entity in EntityStore.Instance.EntityCollection.Values)
-                entity.ValidateAllMembers();
-        }
+        #region IEntityProvider Implementation
 
         public string Name
         {
             get { return "NHibernateManager"; }
         }
 
-        public void Save()
+        public string Description
         {
+            get { return "Entity Provider for PLINQO for NHibernate"; }
         }
 
         public bool Validate()
@@ -113,11 +47,60 @@ namespace CodeSmith.SchemaHelper.NHibernate
             return _entityPaths != null && _entityPaths.Length > 0;
         }
 
-        private bool _intialized = false;
+        public void Load()
+        {
+            Initialize();
 
-        private string[] _entityPaths;
-        private string[] _viewPaths;
-        private string[] _functionPaths;
+            if (_entityPaths != null)
+            {
+                foreach (var file in _entityPaths)
+                {
+                    var doc = ReadHbmDocument(file);
+                    if(doc == null)
+                        continue;
+
+                    var entity = new NHibernateEntity(doc, Path.GetFileName(file));
+                    EntityStore.Instance.EntityCollection.Add(entity.Name, entity);
+                }
+            }
+            if (_viewPaths != null)
+            {
+                foreach (var file in _viewPaths)
+                {
+                    var doc = ReadHbmDocument(file);
+                    if (doc == null)
+                        continue;
+
+                    var entity = new NHibernateEntity(doc, Path.GetFileName(file), true);
+                    EntityStore.Instance.EntityCollection.Add(entity.Name, entity);
+                }
+            }
+
+            if (_functionPaths != null)
+            {
+                foreach (var file in _functionPaths)
+                {
+                    var doc = ReadHbmDocument(file);
+                    if (doc == null)
+                        continue;
+
+                    var entity = new NHibernateCommandEntity(doc, Path.GetFileName(file));
+                    EntityStore.Instance.EntityCollection.Add(entity.Name, entity);
+                }
+            }
+
+            foreach (var entity in EntityStore.Instance.EntityCollection.Values)
+                entity.Initialize();
+
+            foreach (var entity in EntityStore.Instance.EntityCollection.Values)
+                entity.ValidateAllMembers();
+        }
+
+        public void Save()
+        {
+        }
+
+        #endregion
 
         private void Initialize()
         {
@@ -125,15 +108,39 @@ namespace CodeSmith.SchemaHelper.NHibernate
                 return;
 
             if (Directory.Exists(EntitiesDirectory))
-                _entityPaths = Directory.GetFiles(EntitiesDirectory, "*" + NHibernateUtilities.MapExtension);
+                _entityPaths = Directory.GetFiles(EntitiesDirectory, String.Format("*{0}", NHibernateUtilities.MapExtension));
 
             if (Directory.Exists(ViewsDirectory))
-                _viewPaths = Directory.GetFiles(ViewsDirectory, "*" + NHibernateUtilities.MapExtension);
+                _viewPaths = Directory.GetFiles(ViewsDirectory, String.Format("*{0}", NHibernateUtilities.MapExtension));
 
             if (Directory.Exists(FunctionsDirectory))
-                _functionPaths = Directory.GetFiles(FunctionsDirectory, "*" + NHibernateUtilities.MapExtension);
+                _functionPaths = Directory.GetFiles(FunctionsDirectory, String.Format("*{0}", NHibernateUtilities.MapExtension));
 
             _intialized = true;
+        }
+
+        private XDocument ReadHbmDocument(string file)
+        {
+            using (var stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var reader = XmlReader.Create(stream))
+                {
+                    try
+                    {
+                        return XDocument.Load(reader);
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.WriteLine("Unable to load file: " + file);
+                        Trace.WriteLine(ex.ToString());
+
+                        Debug.WriteLine("Unable to load file " + file);
+                        Debug.WriteLine(ex.ToString());
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
