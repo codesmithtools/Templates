4750959c44ef0ba6a775b98dca558a4af88d598b
Blake Niemyjski <bniemyjski@gmail.com>
2012-06-05 20:27:46 +0000

    Added Release tag

--- Projects/CSharp/TypedDataSetSample/readme.txt
+++
@@ -1,3 +0,0 @@
-This project contains a test application for the typed DataSet templates.
-
-** NOTE: These templates are a work in progress.
--- Projects/ConsoleSamples/MergeOutputSample.vb
+++
@@ -1,9 +0,0 @@
-Public Class MergeSample
-    Public Sub SomeCustomMethod()
-        ' This is my custom code that I don't want over-ridden.
-    End Sub
-
-#Region "Sample Generated Region"
-    'This comment will be over-ridden with the output of the template.
-#End Region
-End Class
