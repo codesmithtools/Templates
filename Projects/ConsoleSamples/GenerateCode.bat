6d5369be5e2c5e8b78b04e8c4b2a8b2857b6cc15
Blake Niemyjski <bniemyjski@gmail.com>
2012-06-25 18:09:26 +0000

    Added tag for nhibernate 1.2.1 that compiles against Generator 6.5

--- Projects/ConsoleSamples/GenerateCode.bat
+++
@@ -1,8 +0,0 @@
-@REM ********************************************************************************
-@REM
-@REM This is an example of using the console client to generate the outputs of a
-@REM CodeSmith Generator Project file.  You can add as many outputs as you want to a project.
-@REM
-@REM ********************************************************************************
-
-cs.exe BatchModeSample.csp
