cd19171996dbf8eafb1f7e54a2048da0ae8b76c9
Blake Niemyjski <bniemyjski@gmail.com>
2012-09-27 17:09:59 +0000

    Fixed a issue where vb access modifiers could be incorrectly generated.

--- Templates/Frameworks/PLINQO/Source/Dbml/Naming.cs
+++ Templates/Frameworks/PLINQO/Source/Dbml/Naming.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace LinqToSqlShared.DbmlObjectModel
 {
     public static class Naming
     {
-
         public static string GetModifier(AccessModifier? access)
         {
             return access.ToString().ToLower();
@@ -14,19 +11,46 @@ namespace LinqToSqlShared.DbmlObjectModel
 
         public static string GetModifier(AccessModifier? access, ClassModifier? modifier)
         {
-            if (modifier == null)
-                return access.ToString().ToLower();
-            else
-                return string.Format("{0} {1}", access.ToString().ToLower(), modifier.ToString().ToLower());
+            return !modifier.HasValue
+                       ? access.ToString().ToLower()
+                       : String.Format("{0} {1}", access.ToString().ToLower(), modifier.ToString().ToLower());
         }
 
         public static string GetModifier(AccessModifier? access, MemberModifier? modifier)
         {
-            if (modifier == null)
-                return access.ToString().ToLower();
-            else
-                return string.Format("{0} {1}", access.ToString().ToLower(), modifier.ToString().ToLower());
+            return !modifier.HasValue
+                       ? access.ToString().ToLower()
+                       : String.Format("{0} {1}", access.ToString().ToLower(), modifier.ToString().ToLower());
         }
 
+        public static string GetVisualBasicModifier(AccessModifier? access)
+        {
+            return GetVisualBasicModifier(access.ToString());
+        }
+
+        public static string GetVisualBasicModifier(AccessModifier? access, ClassModifier? modifier)
+        {
+            return !modifier.HasValue
+                       ? GetVisualBasicModifier(access.ToString())
+                       : String.Format("{0} {1}", GetVisualBasicModifier(access.ToString()), modifier);
+        }
+
+        public static string GetVisualBasicModifier(AccessModifier? access, MemberModifier? modifier)
+        {
+            return !modifier.HasValue
+                       ? GetVisualBasicModifier(access.ToString())
+                       : String.Format("{0} {1}", GetVisualBasicModifier(access.ToString()), modifier);
+        }
+
+        private static string GetVisualBasicModifier(string modifier)
+        {
+            if (String.Equals(modifier, "Internal", StringComparison.OrdinalIgnoreCase))
+                return "Friend";
+
+            if (String.Equals(modifier, "ProtectedInternal", StringComparison.OrdinalIgnoreCase))
+                return "Protected Friend";
+
+            return modifier;
+        }
     }
 }
