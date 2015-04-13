c67b273c056135e8c50da9133b7f305eee866542
Blake Niemyjski <bniemyjski@gmail.com>
2015-04-13 22:31:29 +0000

    Added typescript templates from https://github.com/loresoft/TypeScriptGenerator

---
+++ Templates/Other/TypeScript/Interfaces/Generator.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Mono.Cecil;
+
+public static class Generator
+{
+    public static TypeScriptInterface CreateInterface(string assemblyFile, string rootType, bool odata = false)
+    {
+        var moduleDefinition = ModuleDefinition.ReadModule(assemblyFile);
+        if (moduleDefinition == null)
+            throw new ArgumentException(string.Format("The assembly '{0}' could not be loaded.", assemblyFile), "assemblyFile");
+
+        var typeDefintion = moduleDefinition.GetType(rootType);
+        if (typeDefintion == null)
+            throw new ArgumentException(string.Format("The type '{0}' could not be found.", rootType), "rootType");
+
+        var scriptInterface = new TypeScriptInterface();
+        scriptInterface.Name = typeDefintion.Name;
+
+        // get all properties
+        var propertyDefinitions = typeDefintion
+            .Traverse(t => t.BaseType == null ? null : t.BaseType.Resolve())
+            .SelectMany(t => t.Properties);
+
+        foreach (var propertyDefinition in propertyDefinitions)
+        {
+            var scriptPropery = new TypeScriptProperty();
+            scriptPropery.Name = propertyDefinition.Name;
+
+            var propertyType = propertyDefinition.PropertyType;
+            scriptPropery.Type = propertyType.ToScriptType(odata);
+            scriptPropery.IsNullable = propertyDefinition.PropertyType.IsNullable();
+            scriptPropery.IsArray = propertyDefinition.PropertyType.IsScriptArray();
+             scriptInterface.Properties.Add(scriptPropery);
+        }
+
+        return scriptInterface;
+    }
+}
+
+public class TypeScriptInterface
+{
+    public TypeScriptInterface()
+    {
+        Properties = new List<TypeScriptProperty>();
+    }
+
+    public string Name { get; set; }
+
+    public string Extends { get; set; }
+
+    public List<TypeScriptProperty> Properties { get; set; }
+}
+
+public class TypeScriptProperty
+{
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public bool IsNullable { get; set; }
+    public bool IsArray { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("Name: {0}, Type: {1}, Nullable: {2}, Array: {3}", Name, Type, IsNullable, IsArray);
+    }
+}
+
+public static class Extensions
+{
+    public static TypeReference GetUnderlyingType(this TypeReference type)
+    {
+        if (!type.IsGenericInstance)
+            return type;
+
+        var genericType = type as GenericInstanceType;
+        if (genericType == null)
+            return type;
+
+        var genericDefinition = genericType.Resolve();
+        if (genericDefinition.GenericParameters.Count != 1 && genericDefinition.FullName != "System.Nullable`1")
+            return type;
+
+        return genericType.GenericArguments.Single();
+    }
+
+    public static bool HasInterface(this TypeReference typeReference, string fullName)
+    {
+        TypeDefinition definition = typeReference.Resolve();
+
+        return definition.Interfaces
+            .Select(i => i.Resolve())
+            .Any(d => d.FullName == fullName);
+    }
+
+    public static bool IsNullable(this TypeReference type)
+    {
+        bool isNullable = type.IsValueType == false;
+        if (!type.IsGenericInstance)
+            return isNullable;
+
+        var genericType = type as GenericInstanceType;
+        if (genericType == null)
+            return isNullable;
+
+        var genericDefinition = genericType.Resolve();
+        if (genericDefinition.GenericParameters.Count == 1 && genericDefinition.FullName == "System.Nullable`1")
+            return true;
+
+        return isNullable;
+    }
+
+    public static bool IsScriptArray(this TypeReference typeReference)
+    {
+        var baseType = typeReference.GetUnderlyingType();
+        if (baseType.FullName == "System.String" || baseType.FullName == "System.Byte[]")
+            return false;
+
+        if (baseType.IsArray)
+            return true;
+
+        TypeReference elementType = null;
+        var isEnumerable = IsEnumerable(baseType, out elementType);
+        if (isEnumerable)
+            return true;
+
+        return false;
+    }
+    
+    public static bool IsEnumerable(this TypeReference typeReference, out TypeReference elementType)
+    {
+        elementType = null;
+        
+        var collectionType = typeReference
+            .Traverse(t => t.Resolve().BaseType)
+            .FirstOrDefault(t => t.HasInterface("System.Collections.Generic.IEnumerable`1"));
+
+        var genericCollectionType = collectionType as GenericInstanceType;
+        if (genericCollectionType == null)
+            return false;
+
+        //NOTE issue with custom implementation and matching up argument position
+        elementType = genericCollectionType.GenericArguments.Single();
+        return true;
+    }
+
+    public static bool IsCollection(this TypeReference typeReference, out TypeReference elementType)
+    {
+        elementType = null;
+
+        var collectionType = typeReference
+            .Traverse(t => t.Resolve().BaseType)
+            .FirstOrDefault(t => t.HasInterface("System.Collections.Generic.ICollection`1"));
+
+        var genericCollectionType = collectionType as GenericInstanceType;
+        if (genericCollectionType == null)
+            return false;
+
+        //NOTE issue with custom implementation and matching up argument position
+        elementType = genericCollectionType.GenericArguments.Single();
+        return true;
+    }
+
+    public static bool IsDictionary(this TypeReference typeReference, out TypeReference keyType, out TypeReference elementType)
+    {
+        keyType = null;
+        elementType = null;
+
+        var dictionaryType = typeReference
+            .Traverse(t => t.Resolve().BaseType)
+            .FirstOrDefault(t => t.HasInterface("System.Collections.Generic.IDictionary`2"));
+
+        var genericDictionaryType = dictionaryType as GenericInstanceType;
+        if (genericDictionaryType == null)
+            return false;
+
+        //NOTE issue with custom implementation and matching up argument position
+        keyType = genericDictionaryType.GenericArguments.First();
+        elementType = genericDictionaryType.GenericArguments.Last();
+
+        return true;
+    }
+
+    public static string ToScriptType(this TypeReference typeReference, bool odata = false)
+    {
+        var t = typeReference.GetUnderlyingType();
+
+        switch (t.Name)
+        {
+            case "Int16":
+            case "Int32":
+            case "Byte":
+            case "Double":
+            case "SByte":
+            case "Single":
+            case "UInt16":
+            case "UInt32":
+                return "number";
+            case "Decimal":
+            case "Int64":
+            case "UInt64":
+                return odata ? "string" : "number";
+            case "Boolean":
+                return "boolean";
+            case "DateTime":
+            case "DateTimeOffset":
+                return "Date";
+            case "String":
+            case "Guid":
+            case "TimeSpan":
+                return "string";
+            case "Byte[]":
+                return "string";
+            default:
+                return "any";
+        }
+    }
+
+
+    public static IEnumerable<T> Traverse<T>(this T root, Func<T, IEnumerable<T>> childrenSelector)
+    {
+        yield return root;
+
+        IEnumerable<T> descendants = childrenSelector(root);
+        if (descendants == null)
+            yield break;
+
+        foreach (T descendant in descendants.SelectMany(element => element.Traverse(childrenSelector)))
+            yield return descendant;
+    }
+
+    public static IEnumerable<T> Traverse<T>(this T root, Func<T, T> childrenSelector) where T : class
+    {
+        T descendant = root;
+        while (descendant != null)
+        {
+            yield return descendant;
+            descendant = childrenSelector(descendant);
+        }
+    }
+
+
+    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+    {
+        return source.DistinctBy(keySelector, EqualityComparer<TKey>.Default);
+    }
+
+    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
+    {
+        if (source == null)
+            throw new ArgumentNullException("source");
+        if (keySelector == null)
+            throw new ArgumentNullException("keySelector");
+        if (comparer == null)
+            throw new ArgumentNullException("comparer");
+
+        var knownKeys = new HashSet<TKey>(comparer);
+        return source.Where(element => knownKeys.Add(keySelector(element)));
+    }
+}
---
+++ Templates/Other/TypeScript/Interfaces/README.md
@@ -0,0 +1,37 @@
+#TypeScriptGenerator
+
+CodeSmith Template to generate TypeScript interface for .NET a class
+
+[![Build status](https://ci.appveyor.com/api/projects/status/5wonpaggu8m843wd/branch/master?svg=true)](https://ci.appveyor.com/project/LoreSoft/typescriptgenerator/branch/master)
+
+##Download
+
+The templates are available on nuget.org via package name `TypeScriptGenerator`.
+
+To install TypeScriptGenerator, run the following command in the Package Manager Console
+
+    PM> Install-Package TypeScriptGenerator
+    
+More information about NuGet package avaliable at
+https://nuget.org/packages/TypeScriptGenerator
+
+##Development Builds
+
+Development builds are available on the myget.org feed.  A development build is promoted to the main NuGet feed when it's determined to be stable. 
+
+In your Package Manager settings add the following package source for development builds:
+http://www.myget.org/F/loresoft/
+
+
+## License
+
+Copyright (c) 2014, LoreSoft
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+- Neither the name of LoreSoft nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
